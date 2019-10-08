47eed3cc6594a6e0c44bf0e6142961822c4edacd
Stephan Lukasczyk <stephan@lukasczyk.me>
2019-10-08 09:53:39 +0200

    Add custom exception types

---
+++ pynguin/utils/exceptions.py
@@ -0,0 +1,27 @@
+# This file is part of Pynguin.
+#
+# Pynguin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pynguin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pynguin.  If not, see <https://www.gnu.org/licenses/>.
+"""Provides custom exception types."""
+
+
+class ConfigurationException(BaseException):
+    """An exception type that's raised if the generator has no proper configuration."""
+
+
+class GenerationException(BaseException):
+    """An exception during test generation.
+
+    This type shall be used for all exceptions that occur during test generation and
+    that are caused by the test-generation process.
+    """
---
+++ tests/utils/test_exceptions.py
@@ -0,0 +1,39 @@
+# This file is part of Pynguin.
+#
+# Pynguin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pynguin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pynguin.  If not, see <https://www.gnu.org/licenses/>.
+import pytest
+
+from pynguin.utils.exceptions import GenerationException, ConfigurationException
+
+
+def test_raise_test_generation_exception():
+    with pytest.raises(GenerationException):
+        raise GenerationException()
+
+
+def test_raise_configuration_exception():
+    with pytest.raises(ConfigurationException):
+        raise ConfigurationException()
+
+
+def test_raise_test_generation_exception_with_message():
+    with pytest.raises(GenerationException) as exception:
+        raise GenerationException("foo")
+    assert exception.value.args[0] == "foo"
+
+
+def test_raise_configuration_exception_with_message():
+    with pytest.raises(ConfigurationException) as exception:
+        raise ConfigurationException("foo")
+    assert exception.value.args[0] == "foo"
