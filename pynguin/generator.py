403bebc8c3aa74de24406cde1886873d5c14af1e
Stephan Lukasczyk <stephan@lukasczyk.me>
2019-10-08 10:11:04 +0200

    Start implementation generator interface

---
+++ pynguin/generator.py
@@ -0,0 +1,101 @@
+# This file is part of Pynguin.
+#
+# Pynguin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pynguin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pynguin.  If not, see <https://www.gnu.org/licenses/>.
+"""Entry"""
+import argparse
+import logging
+import os
+
+from typing import Union, List
+
+from pynguin.configuration import Configuration, ConfigurationBuilder
+from pynguin.utils.exceptions import ConfigurationException
+
+
+class TestGenerator:
+    """The basic interface of the test generator."""
+
+    def __init__(
+        self,
+        argument_parser: argparse.ArgumentParser = None,
+        arguments: List[str] = None,
+        configuration: Configuration = None,
+    ) -> None:
+        """Initialises the test generator.
+
+        The generator needs a configuration, which can either be provided via the
+        `configuration` parameter or via an argument parser and a list of
+        command-line arguments.  If none of these is present, the generator cannot be
+        initialised and will thus raise a `ConfigurationException`.
+
+        :param argument_parser: An optional argument parser.
+        :param arguments: An optional list of command-line arguments.
+        :param configuration: An optional pre-generated configuration.
+        :raises ConfigurationException: In case there is no proper configuration
+        """
+        if configuration:
+            self._configuration = configuration
+        elif argument_parser and arguments:
+            self._configuration = ConfigurationBuilder.build_from_cli_arguments(
+                argument_parser, arguments
+            )
+        else:
+            raise ConfigurationException(
+                "Cannot initialise test generator without proper configuration."
+            )
+
+    def setup(self) -> None:
+        """Setup"""
+
+    @staticmethod
+    def run() -> int:
+        """Run"""
+        return 1
+
+    @staticmethod
+    def _setup_logging(
+        verbose: bool = False,
+        quiet: bool = False,
+        log_file: Union[str, os.PathLike] = None,
+    ) -> logging.Logger:
+        logger = logging.getLogger("pynguin")
+        logger.setLevel(logging.DEBUG)
+        if verbose:
+            level = logging.DEBUG
+        elif quiet:
+            level = logging.NOTSET
+        else:
+            level = logging.INFO
+        if log_file:
+            file_handler = logging.FileHandler(log_file)
+            file_handler.setFormatter(
+                logging.Formatter(
+                    "%(asctime)s [%(levelname)s](%(name)s:%(funcName)s:%(lineno)d: "
+                    "%(message)s"
+                )
+            )
+            file_handler.setLevel(logging.DEBUG)
+            logger.addHandler(file_handler)
+
+        if not quiet:
+            console_handler = logging.StreamHandler()
+            console_handler.setLevel(level)
+            console_handler.setFormatter(
+                logging.Formatter("[%(levelname)s](%(name)s): %(message)s")
+            )
+            logger.addHandler(console_handler)
+        else:
+            logger.addHandler(logging.NullHandler())
+
+        return logger
---
+++ tests/test_generator.py
@@ -0,0 +1,91 @@
+# This file is part of Pynguin.
+#
+# Pynguin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pynguin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pynguin.  If not, see <https://www.gnu.org/licenses/>.
+import importlib
+import logging
+import os
+import tempfile
+from argparse import ArgumentParser
+from unittest import mock
+from unittest.mock import MagicMock
+
+import pytest
+
+from pynguin.configuration import Configuration
+from pynguin.generator import TestGenerator
+from pynguin.utils.exceptions import ConfigurationException
+
+
+def test__setup_logging_standard_with_log_file():
+    _, log_file = tempfile.mkstemp()
+    logging.shutdown()
+    importlib.reload(logging)
+    logger = TestGenerator._setup_logging(log_file=log_file)
+    assert isinstance(logger, logging.Logger)
+    assert logger.level == logging.DEBUG
+    assert len(logger.handlers) == 2
+    os.remove(log_file)
+    logging.shutdown()
+    importlib.reload(logging)
+
+
+def test__setup_logging_verbose_without_log_file():
+    logging.shutdown()
+    importlib.reload(logging)
+    logger = TestGenerator._setup_logging(verbose=True)
+    assert len(logger.handlers) == 1
+    assert logger.handlers[0].level == logging.DEBUG
+    logging.shutdown()
+    importlib.reload(logging)
+
+
+def test__setup_logging_quiet_without_log_file():
+    logging.shutdown()
+    importlib.reload(logging)
+    logger = TestGenerator._setup_logging(quiet=True)
+    assert len(logger.handlers) == 1
+    assert isinstance(logger.handlers[0], logging.NullHandler)
+    logging.shutdown()
+    importlib.reload(logging)
+
+
+def test_init_with_configuration():
+    configuration = MagicMock(Configuration)
+    generator = TestGenerator(configuration=configuration)
+    assert generator._configuration == configuration
+
+
+def test_init_without_params():
+    with pytest.raises(ConfigurationException) as exception:
+        TestGenerator()
+    assert (
+        exception.value.args[0] == "Cannot initialise test generator without "
+        "proper configuration."
+    )
+
+
+def test_init_with_cli_arguments():
+    parser = MagicMock(ArgumentParser)
+    args = [""]
+    with mock.patch(
+        "pynguin.generator.ConfigurationBuilder.build_from_cli_arguments"
+    ) as builder_mock:
+        builder_mock.return_value = 42
+        generator = TestGenerator(argument_parser=parser, arguments=args)
+        assert generator._configuration == 42
+
+
+def test_run():
+    generator = TestGenerator(configuration=MagicMock(Configuration))
+    assert generator.run() == 1
