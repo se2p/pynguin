7cd07a04e0e50bcc4bc9652d959c08e3e7694224
Stephan Lukasczyk <stephan@lukasczyk.me>
2020-01-16 15:06:55 +0100

    Add instance check for clone result

--- tests/testcase/test_defaulttestcase.py
+++ tests/testcase/test_defaulttestcase.py
@@ -196,6 +196,7 @@ def test_clone(default_test_case):
     stmt.return_value.clone.return_value = ref
     default_test_case._statements = [stmt]
     result = default_test_case.clone()
+    assert isinstance(result, dtc.DefaultTestCase)
     assert result.id != default_test_case.id
     assert result.size() == 1
     assert result.get_statement(0) == stmt
