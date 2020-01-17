346734a70fd292522ee3f8b773fb84184cc8b4d2
Stephan Lukasczyk <stephan@lukasczyk.me>
2020-01-17 12:09:27 +0100

    Provide test for primitive statement types

---
+++ tests/testcase/statements/test_primitivestatements.py
@@ -0,0 +1,92 @@
+# This file is part of Pynguin.
+#
+# Pynguin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pynguin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pynguin.  If not, see <https://www.gnu.org/licenses/>.
+from unittest.mock import MagicMock
+
+import pytest
+
+import pynguin.testcase.statements.primitivestatements as prim
+import pynguin.testcase.testcase as tc
+
+
+@pytest.mark.parametrize(
+    "statement_type,test_case,value",
+    [
+        pytest.param(prim.IntPrimitiveStatement, MagicMock(tc.TestCase), 42),
+        pytest.param(prim.FloatPrimitiveStatement, MagicMock(tc.TestCase), 42.23),
+        pytest.param(prim.StringPrimitiveStatement, MagicMock(tc.TestCase), "foo"),
+        pytest.param(prim.BooleanPrimitiveStatement, MagicMock(tc.TestCase), True),
+    ],
+)
+def test_primitive_statement_value(statement_type, test_case, value):
+    statement = statement_type(test_case, value)
+    assert statement.value == value
+
+
+@pytest.mark.parametrize(
+    "statement_type,test_case,value,new_value",
+    [
+        pytest.param(prim.IntPrimitiveStatement, MagicMock(tc.TestCase), 42, 23),
+        pytest.param(prim.FloatPrimitiveStatement, MagicMock(tc.TestCase), 2.1, 1.2),
+        pytest.param(
+            prim.StringPrimitiveStatement, MagicMock(tc.TestCase), "foo", "bar"
+        ),
+        pytest.param(
+            prim.BooleanPrimitiveStatement, MagicMock(tc.TestCase), True, False
+        ),
+    ],
+)
+def test_primitive_statement_set_value(statement_type, test_case, value, new_value):
+    statement = statement_type(test_case, value)
+    statement.value = new_value
+    assert statement.value == new_value
+
+
+@pytest.mark.parametrize(
+    "statement_type,test_case,new_test_case,value",
+    [
+        pytest.param(
+            prim.IntPrimitiveStatement,
+            MagicMock(tc.TestCase),
+            MagicMock(tc.TestCase),
+            42,
+        ),
+        pytest.param(
+            prim.FloatPrimitiveStatement,
+            MagicMock(tc.TestCase),
+            MagicMock(tc.TestCase),
+            42.23,
+        ),
+        pytest.param(
+            prim.StringPrimitiveStatement,
+            MagicMock(tc.TestCase),
+            MagicMock(tc.TestCase),
+            "foo",
+        ),
+        pytest.param(
+            prim.BooleanPrimitiveStatement,
+            MagicMock(tc.TestCase),
+            MagicMock(tc.TestCase),
+            True,
+        ),
+    ],
+)
+def test_primitive_statement_clone(statement_type, test_case, new_test_case, value):
+    statement = statement_type(test_case, value)
+    new_statement = statement.clone(new_test_case)
+    assert new_statement.test_case == new_test_case
+    assert (
+        new_statement.return_value.variable_type == statement.return_value.variable_type
+    )
+    assert new_statement.value == statement.value
