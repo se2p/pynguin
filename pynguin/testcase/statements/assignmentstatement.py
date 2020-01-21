0bfd8c1e3b27fe0e34a7c42cb11ca7e9003f4a13
Florian Kroiss <kroiss@fim.uni-passau.de>
2020-01-21 13:57:34 +0100

    Add assignment statement with minimal tests

---
+++ pynguin/testcase/statements/assignmentstatement.py
@@ -0,0 +1,38 @@
+# This file is part of Pynguin.
+#
+# Pynguin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pynguin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pynguin.  If not, see <https://www.gnu.org/licenses/>.
+"""
+Provide a statement that performs assignments.
+"""
+import pynguin.testcase.statements.statement as stmt
+import pynguin.testcase.testcase as tc
+import pynguin.testcase.variable.variablereference as vr
+
+
+class AssignmentStatement(stmt.Statement):
+    """
+    A statement that assigns one variable to another.
+    """
+
+    def __init__(
+        self,
+        test_case: tc.TestCase,
+        lhs: vr.VariableReference,
+        rhs: vr.VariableReference,
+    ):
+        super().__init__(test_case, lhs)
+        self._rhs = rhs
+
+    def clone(self, test_case: tc.TestCase) -> stmt.Statement:
+        pass
