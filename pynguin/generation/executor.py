4f88f76d30fc5dcaef9056c6d0a9f563dad0193e
Florian Kroiss <kroiss@fim.uni-passau.de>
2020-01-23 17:35:40 +0100

    Add simple skeleton for executor

--- pynguin/generation/executor.py
+++ pynguin/generation/executor.py
@@ -13,10 +13,14 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Pynguin.  If not, see <https://www.gnu.org/licenses/>.
 """Provides an executor that executes generated sequences."""
+import ast
 import logging
-from typing import Tuple, Union, Any
+from typing import Tuple, Union, Any, List
+
+import astor
 
 import pynguin.testcase.testcase as tc
+import pynguin.testcase.statement_to_ast as stmt_to_ast
 from pynguin.utils.proxy import MagicProxy
 
 
@@ -44,3 +48,36 @@ class Executor:
         :param test_case: The test case that shall be executed
         :return: Whether or not the execution was successful
         """
+        # TODO(fk) wrap new values in magic proxy.
+        local_namespace = {}
+
+        # TODO(fk) Provide required global stuff/modules.
+        # TODO(fk) Provide capabilities to add instrumentation/tracing
+        global_namespace = {}
+        for node in self._to_ast_nodes(test_case):
+            try:
+                co = compile(self._wrap_node_in_module(node), "<ast>", 'exec')
+                exec(co, global_namespace, local_namespace)
+            except Exception as err:
+                failed_stmt = astor.to_source(node)
+                Executor._logger.warning(f"Failed to execute statement\n{failed_stmt}{err.args}")
+                return False
+        return True
+        # TODO(fk) Provide ExecutionResult with more information(coverage, fitness, etc.), not just True/False
+
+    @staticmethod
+    def _to_ast_nodes(test_case: tc.TestCase) -> List[ast.AST]:
+        """Transforms the given test case into a list of ast nodes."""
+        naming_scope = stmt_to_ast.NamingScope()
+        visitor = stmt_to_ast.StatementToAstVisitor(naming_scope)
+        for statement in test_case.statements:
+            statement.accept(visitor)
+        return visitor.ast_nodes
+
+    @staticmethod
+    def _wrap_node_in_module(node: ast.AST) -> ast.Module:
+        """Wraps the given node in a Module, so that it can be executed."""
+        ast.fix_missing_locations(node)
+        wrapper = ast.parse("")
+        wrapper.body = [node]
+        return wrapper
---
+++ tests/generation/test_executor_integration.py
@@ -0,0 +1,37 @@
+# This file is part of Pynguin.
+#
+# Pynguin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pynguin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pynguin.  If not, see <https://www.gnu.org/licenses/>.
+"""Integration tests for the executor."""
+
+from pynguin.generation.executor import Executor
+import pynguin.testcase.defaulttestcase as dtc
+import pynguin.testcase.statements.primitivestatements as prim_stmt
+import pynguin.testcase.statements.parametrizedstatements as param_stmt
+
+
+def test_simple_execution():
+    test_case = dtc.DefaultTestCase()
+    test_case.add_statement(prim_stmt.IntPrimitiveStatement(test_case, 5))
+    executor = Executor()
+    assert executor.execute(test_case)
+
+
+def test_illegal_call():
+    test_case = dtc.DefaultTestCase()
+    int_stmt = prim_stmt.IntPrimitiveStatement(test_case, 5)
+    method_stmt = param_stmt.MethodStatement(test_case, str, "i_dont_exist", int_stmt.return_value)
+    test_case.add_statement(int_stmt)
+    test_case.add_statement(method_stmt)
+    executor = Executor()
+    assert not executor.execute(test_case)
