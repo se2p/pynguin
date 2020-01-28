4852b5f293580042322df1e09ab4020b2f7f6f83
Florian Kroiss <kroiss@fim.uni-passau.de>
2020-01-28 19:38:16 +0100

    Add own random instance that can be seeded.

---
+++ pynguin/utils/randomness.py
@@ -0,0 +1,32 @@
+# This file is part of Pynguin.
+#
+# Pynguin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pynguin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pynguin.  If not, see <https://www.gnu.org/licenses/>.
+"""Provides a singleton instance of Random that can be seeded."""
+import random
+import string
+
+RNG: random.Random = random.Random()
+
+
+def next_char() -> str:
+    """Create a random printable ascii char."""
+    return RNG.choice(string.printable)
+
+
+def next_string(length: int) -> str:
+    """
+    Create a random string consisting of printable and with the given length.
+    :param length the desired length
+    """
+    return "".join(next_char() for _ in range(length))
---
+++ tests/utils/test_randomness.py
@@ -0,0 +1,30 @@
+# This file is part of Pynguin.
+#
+# Pynguin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pynguin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pynguin.  If not, see <https://www.gnu.org/licenses/>.
+import string
+
+import pynguin.utils.randomness as randomness
+
+
+def test_next_char_printable():
+    assert randomness.next_char() in string.printable
+
+
+def test_next_string_length():
+    assert len(randomness.next_string(15)) == 15
+
+
+def test_next_string_printable():
+    rand = randomness.next_string(15)
+    assert all(char in string.printable for char in rand)
