67bd8b2f3802b43f0159d2b8ddff37cc6ac56658
Stephan Lukasczyk <stephan@lukasczyk.me>
2020-02-20 13:13:34 +0100

    Extract abstract executor
    Execution cannot only be needed to check whether generated sequences are
    executable, or which coverage they achieve, but also for other reasons.
    An example would be to execute a type checker or a type inference
    engine, such as mypy or MonkeyType.  The abstract class provides basic
    functionality and an interface to allow such extensions of the
    framework.

---
+++ pynguin/testcase/execution/abstractexecutor.py
@@ -0,0 +1,97 @@
+# This file is part of Pynguin.
+#
+# Pynguin is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pynguin is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Pynguin.  If not, see <https://www.gnu.org/licenses/>.
+"""Provides an abstract executor as a base class for various executors."""
+import ast
+import sys
+from abc import ABCMeta, abstractmethod
+from typing import Generic, TypeVar, List, Dict, Any
+
+import pynguin.testcase.statement_to_ast as stmt_to_ast
+import pynguin.testcase.testcase as tc
+
+T = TypeVar("T")  # pylint: disable=invalid-name
+
+
+class AbstractExecutor(Generic[T], metaclass=ABCMeta):
+    """An abstract executor that executes the generated test cases."""
+
+    def __init__(self) -> None:
+        self._local_namespace: Dict[str, Any] = {}
+        self._variable_names = stmt_to_ast.NamingScope()
+        self._modules_aliases = stmt_to_ast.NamingScope(prefix="module")
+        self._ast_nodes: List[ast.stmt] = []
+        self._global_namespace: Dict[str, Any] = {}
+
+    @abstractmethod
+    def execute(self, test_case: tc.TestCase) -> T:
+        """Executes the statements in a test case.
+
+        :param test_case: The test case that shall be executed
+        :return: Result of the execution
+        """
+
+    @abstractmethod
+    def execute_test_suite(self, test_suite: List[tc.TestCase]) -> T:
+        """Executes all statements of all test cases in a test suite.
+
+        :param test_suite: The list of test cases, i.e., test test suite
+        :return: Result of the execution
+        """
+
+    @staticmethod
+    def to_ast_nodes(
+        test_case: tc.TestCase,
+        variable_names: stmt_to_ast.NamingScope,
+        modules_aliases: stmt_to_ast.NamingScope,
+    ) -> List[ast.stmt]:
+        """Transforms the given test case into a list of ast nodes.
+
+        :param test_case: The current test case
+        :param variable_names: The scope of the variable names
+        :param modules_aliases: The cope of the module alias names
+        :return: A list of ast nodes
+        """
+        visitor = stmt_to_ast.StatementToAstVisitor(modules_aliases, variable_names)
+        for statement in test_case.statements:
+            statement.accept(visitor)
+        return visitor.ast_nodes
+
+    @staticmethod
+    def wrap_node_in_module(node: ast.stmt) -> ast.Module:
+        """Wraps the given node in a module, such that it can be executed.
+
+        :param node: The node to wrap
+        :return: The module wrapping the node
+        """
+        ast.fix_missing_locations(node)
+        wrapper = ast.parse("")
+        wrapper.body = [node]
+        return wrapper
+
+    @staticmethod
+    def prepare_global_namespace(
+        modules_aliases: stmt_to_ast.NamingScope,
+    ) -> Dict[str, Any]:
+        """Provides the required modules under the given aliases.
+
+        :param modules_aliases: The module aliases
+        :return: A dictionary of module aliases and the corresponding module
+        """
+        global_namespace: Dict[str, Any] = {}
+        for required_module in modules_aliases.known_name_indices:
+            global_namespace[modules_aliases.get_name(required_module)] = sys.modules[
+                required_module
+            ]
+        return global_namespace
--- pynguin/testcase/execution/testcaseexecutor.py
+++ pynguin/testcase/execution/testcaseexecutor.py
@@ -13,22 +13,22 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Pynguin.  If not, see <https://www.gnu.org/licenses/>.
 """Provides an executor that executes generated sequences."""
-import ast
 import contextlib
 import importlib
 import logging
 import os
 import sys
-from typing import Tuple, Union, Any, List, Dict, Optional
+from typing import Tuple, Union, Any, List, Optional
 
 import astor
 from coverage import Coverage, CoverageException, CoverageData
 
-import pynguin.testcase.testcase as tc
-import pynguin.testcase.statement_to_ast as stmt_to_ast
-import pynguin.testcase.execution.executionresult as res
 import pynguin.configuration as config
+import pynguin.testcase.execution.executionresult as res
+import pynguin.testcase.statement_to_ast as stmt_to_ast
+import pynguin.testcase.testcase as tc
 from pynguin.instrumentation.basis import get_tracer
+from pynguin.testcase.execution.abstractexecutor import AbstractExecutor
 from pynguin.utils.proxy import MagicProxy
 
 
@@ -41,14 +41,14 @@ def _recording_isinstance(
     return isinstance(obj, obj_type)
 
 
-# pylint: disable=too-few-public-methods
-class TestCaseExecutor:
+class TestCaseExecutor(AbstractExecutor):
     """An executor that executes the generated test cases."""
 
     _logger = logging.getLogger(__name__)
 
     def __init__(self):
         """Initializes the executor. Loads the module under test."""
+        super().__init__()
         if config.INSTANCE.measure_coverage:
             self._coverage = Coverage(
                 branch=True, config_file=False, source=[config.INSTANCE.module_name]
@@ -93,29 +93,18 @@ class TestCaseExecutor:
             self._coverage.get_data().update(self._import_coverage)
 
         # TODO(fk) wrap new values in magic proxy.
-        local_namespace: Dict[str, Any] = {}
-
-        variable_names = stmt_to_ast.NamingScope()
-        modules_aliases = stmt_to_ast.NamingScope(prefix="module")
-        ast_nodes: List[ast.stmt] = TestCaseExecutor._to_ast_nodes(
-            test_case, variable_names, modules_aliases
-        )
-        global_namespace: Dict[str, Any] = TestCaseExecutor._prepare_global_namespace(
-            modules_aliases
-        )
+        self._setup(test_case)
         with open(os.devnull, mode="w") as null_file:
             with contextlib.redirect_stdout(null_file):
-                self._execute_ast_nodes(
-                    ast_nodes, global_namespace, local_namespace, result
-                )
+                self._execute_ast_nodes(result)
                 self._collect_coverage(result)
                 self._collect_fitness(result)
         return result
 
-    def execute_test_suite(self, test_cases: List[tc.TestCase]) -> res.ExecutionResult:
+    def execute_test_suite(self, test_suite: List[tc.TestCase]) -> res.ExecutionResult:
         """Executes all statements of all test cases in a test suite.
 
-        :param test_cases: The list of test cases, i.e., the test suite
+        :param test_suite: The list of test cases, i.e., the test suite
         :return: Result of the execution
         """
         TestCaseExecutor._logger.info(
@@ -129,39 +118,34 @@ class TestCaseExecutor:
 
         with open(os.devnull, mode="w") as null_file:
             with contextlib.redirect_stdout(null_file):
-                for test_case in test_cases:
-                    local_namespace: Dict[str, Any] = {}
-                    variable_names = stmt_to_ast.NamingScope()
-                    modules_aliases = stmt_to_ast.NamingScope(prefix="module")
-                    ast_nodes: List[ast.stmt] = TestCaseExecutor._to_ast_nodes(
-                        test_case, variable_names, modules_aliases
-                    )
-                    global_namespace: Dict[
-                        str, Any
-                    ] = TestCaseExecutor._prepare_global_namespace(modules_aliases)
-                    self._execute_ast_nodes(
-                        ast_nodes, global_namespace, local_namespace, result
-                    )
+                for test_case in test_suite:
+                    self._setup(test_case)
+                    self._execute_ast_nodes(result)
                 self._collect_coverage(result)
                 self._collect_fitness(result)
         TestCaseExecutor._logger.info("Finished re-execution of generated test suite")
         return result
 
+    def _setup(self, test_case: tc.TestCase) -> None:
+        self._local_namespace = {}
+        self._variable_names = stmt_to_ast.NamingScope()
+        self._modules_aliases = stmt_to_ast.NamingScope(prefix="module")
+        self._ast_nodes = self.to_ast_nodes(
+            test_case, self._variable_names, self._modules_aliases
+        )
+        self._global_namespace = self.prepare_global_namespace(self._modules_aliases)
+
     def _execute_ast_nodes(
-        self,
-        ast_nodes: List[ast.stmt],
-        global_namespace: Dict[str, Any],
-        local_namespace: Dict[str, Any],
-        result: res.ExecutionResult,
+        self, result: res.ExecutionResult,
     ):
-        for idx, node in enumerate(ast_nodes):
+        for idx, node in enumerate(self._ast_nodes):
             try:
                 self._logger.debug("Executing %s", astor.to_source(node))
-                code = compile(self._wrap_node_in_module(node), "<ast>", "exec")
+                code = compile(self.wrap_node_in_module(node), "<ast>", "exec")
                 if config.INSTANCE.measure_coverage:
                     self._coverage.start()
                 # pylint: disable=exec-used
-                exec(code, global_namespace, local_namespace)
+                exec(code, self._global_namespace, self._local_namespace)
             except Exception as err:  # pylint: disable=broad-except
                 failed_stmt = astor.to_source(node)
                 TestCaseExecutor._logger.info(
@@ -196,39 +180,3 @@ class TestCaseExecutor:
             tracer = get_tracer(sys.modules[config.INSTANCE.module_name])
             result.fitness = tracer.get_fitness()
             tracer.clear_tracking()
-
-    @staticmethod
-    def _to_ast_nodes(
-        test_case: tc.TestCase,
-        variable_names: stmt_to_ast.NamingScope,
-        modules_aliases: stmt_to_ast.NamingScope,
-    ) -> List[ast.stmt]:
-        """Transforms the given test case into a list of ast nodes."""
-        visitor = stmt_to_ast.StatementToAstVisitor(modules_aliases, variable_names)
-        for statement in test_case.statements:
-            statement.accept(visitor)
-        return visitor.ast_nodes
-
-    @staticmethod
-    def _wrap_node_in_module(node: ast.stmt) -> ast.Module:
-        """Wraps the given node in a module, so that it can be executed."""
-        ast.fix_missing_locations(node)
-        wrapper = ast.parse("")
-        wrapper.body = [node]
-        return wrapper
-
-    @staticmethod
-    def _prepare_global_namespace(
-        modules_aliases: stmt_to_ast.NamingScope,
-    ) -> Dict[str, Any]:
-        """
-        Provides the required modules under the given aliases.
-        :param modules_aliases:
-        :return: a dict of module aliases and the corresponding module.
-        """
-        global_namespace: Dict[str, Any] = {}
-        for required_module in modules_aliases.known_name_indices:
-            global_namespace[modules_aliases.get_name(required_module)] = sys.modules[
-                required_module
-            ]
-        return global_namespace
