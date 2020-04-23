se2p/pynguin

commit d7e83f739e19a9ae339be6d589b66b136cfaab72

BranchDistanceSuiteFitness: Remove exception penalty. Also change tracking to track combined chromosomes as current individual.

pynguin/generator.py
@@ -159,34 +159,34 @@ class Pynguin:
             algorithm: TestGenerationStrategy = self._instantiate_test_generation_strategy(
                 executor, test_cluster
             )
-            test_chromosome, failing_test_chromosome = algorithm.generate_sequences()
+            non_failing, failing = algorithm.generate_sequences()
             algorithm.send_statistics()
 
             with Timer(name="Re-execution time", logger=None):
-                test_suite = tsc.TestSuiteChromosome()
-                for fitness_func in test_chromosome.get_fitness_functions():
-                    test_suite.add_fitness_function(fitness_func)
-                test_suite.add_tests(test_chromosome.test_chromosomes)
-                test_suite.add_tests(failing_test_chromosome.test_chromosomes)
+                combined = tsc.TestSuiteChromosome()
+                for fitness_func in non_failing.get_fitness_functions():
+                    combined.add_fitness_function(fitness_func)
+                combined.add_tests(non_failing.test_chromosomes)
+                combined.add_tests(failing.test_chromosomes)
                 StatisticsTracker().track_output_variable(
-                    RuntimeVariable.Coverage, test_suite.get_coverage()
+                    RuntimeVariable.Coverage, combined.get_coverage()
                 )
 
             export_timer = Timer(name="Export time", logger=None)
             export_timer.start()
             self._logger.info("Export successful test cases")
-            self._export_test_cases(test_chromosome.test_chromosomes)
+            self._export_test_cases(non_failing.test_chromosomes)
             self._logger.info("Export failing test cases")
             self._export_test_cases(
-                failing_test_chromosome.test_chromosomes, "_failing", wrap_code=True
+                failing.test_chromosomes, "_failing", wrap_code=True
             )
             export_timer.stop()
-            self._track_statistics(test_chromosome, failing_test_chromosome)
+            self._track_statistics(combined, failing)
             timer.stop()
             self._collect_statistics()
             if not StatisticsTracker().write_statistics():
                 self._logger.error("Failed to write statistics data")
-            if test_chromosome.size == 0:
+            if non_failing.size == 0:
                 # not able to generate one successful test case
                 status = 1
 
@@ -224,21 +224,17 @@ class Pynguin:
 
     @staticmethod
     def _track_statistics(
-        test_chromosome: tsc.TestSuiteChromosome,
-        failing_test_chromosome: tsc.TestSuiteChromosome,
+        combined: tsc.TestSuiteChromosome, failing: tsc.TestSuiteChromosome,
     ) -> None:
         tracker = StatisticsTracker()
-        tracker.current_individual(test_chromosome)
-        tracker.track_output_variable(RuntimeVariable.Size, test_chromosome.size())
+        tracker.current_individual(combined)
+        tracker.track_output_variable(RuntimeVariable.Size, combined.size())
         tracker.track_output_variable(
-            RuntimeVariable.Length, test_chromosome.total_length_of_test_cases
+            RuntimeVariable.Length, combined.total_length_of_test_cases
         )
+        tracker.track_output_variable(RuntimeVariable.FailingSize, failing.size())
         tracker.track_output_variable(
-            RuntimeVariable.FailingSize, failing_test_chromosome.size()
-        )
-        tracker.track_output_variable(
-            RuntimeVariable.FailingLength,
-            failing_test_chromosome.total_length_of_test_cases,
+            RuntimeVariable.FailingLength, failing.total_length_of_test_cases,
         )
 
     @staticmethod

pynguin/ga/fitnessfunctions/branchdistancesuitefitness.py
@@ -30,21 +30,16 @@ class BranchDistanceSuiteFitnessFunction(asff.AbstractSuiteFitnessFunction):
         self, individual: tsc.TestSuiteChromosome,
     ) -> ff.FitnessValues:
         results = self._run_test_suite(individual)
-        has_exception, merged_trace = self.analyze_traces(results)
+        _, merged_trace = self.analyze_traces(results)
         tracer: ExecutionTracer = self._executor.get_tracer()
 
         return ff.FitnessValues(
-            self._compute_fitness(has_exception, merged_trace, tracer.get_known_data()),
+            self._compute_fitness(merged_trace, tracer.get_known_data()),
             self._compute_coverage(merged_trace, tracer.get_known_data()),
         )
 
     @staticmethod
-    def _compute_fitness(
-        has_exception: bool, trace: ExecutionTrace, known_data: KnownData
-    ) -> float:
-        if has_exception:
-            return BranchDistanceSuiteFitnessFunction.get_worst_fitness(known_data)
-
+    def _compute_fitness(trace: ExecutionTrace, known_data: KnownData) -> float:
         # Check if all code objects were entered.
         code_objects_missing: float = len(known_data.existing_code_objects) - len(
             trace.covered_code_objects
@@ -122,7 +117,8 @@ class BranchDistanceSuiteFitnessFunction(asff.AbstractSuiteFitnessFunction):
 
     @staticmethod
     def get_worst_fitness(known_data: KnownData) -> float:
-        """Compute the worst possible fitness value."""
+        """Compute the worst possible fitness value.
+        Can be used to penalize time outs."""
         return (
             len(known_data.existing_code_objects)
             + len(known_data.existing_predicates) * 2

pynguin/generation/algorithms/wspy/wholesuiteteststrategy.py
@@ -73,6 +73,7 @@ class WholeSuiteTestStrategy(TestGenerationStrategy):
             and self._get_best_individual().get_fitness() != 0.0
         ):
             self.evolve()
+            StatisticsTracker().current_individual(self._get_best_individual())
             self._logger.info(
                 "Generation: %5i. Best fitness: %5f, Best coverage %5f",
                 generation,
@@ -80,7 +81,7 @@ class WholeSuiteTestStrategy(TestGenerationStrategy):
                 self._get_best_individual().get_coverage(),
             )
             generation += 1
-        return self._get_best_individual(), tsc.TestSuiteChromosome()
+        return self.split_chromosomes()
 
     def evolve(self):
         """Evolve the current population and replace it with a new one."""
@@ -162,3 +163,29 @@ class WholeSuiteTestStrategy(TestGenerationStrategy):
         for idx in range(config.INSTANCE.elite):
             elite.append(self._population[idx].clone())
         return elite
+
+    def split_chromosomes(
+        self,
+    ) -> Tuple[tsc.TestSuiteChromosome, tsc.TestSuiteChromosome]:
+        """Split the chromosome into two chromosomes.
+        The first one contains the non failing test cases.
+        The second one contains the failing test cases."""
+        best = self._get_best_individual()
+        # Make sure all test cases have a cached result.
+        best.get_fitness()
+        non_failing = tsc.TestSuiteChromosome()
+        failing = tsc.TestSuiteChromosome()
+
+        for fitness_function in self._fitness_functions:
+            non_failing.add_fitness_function(fitness_function)
+            failing.add_fitness_function(fitness_function)
+
+        for test_case in best.test_chromosomes:
+            result = test_case.get_last_execution_result()
+            assert result is not None
+            if result.has_test_exceptions():
+                failing.add_test(test_case.clone())
+            else:
+                non_failing.add_test(test_case.clone())
+
+        return non_failing, failing

tests/ga/fitnessfunctions/test_branchdistancesuitefitness.py
@@ -44,7 +44,7 @@ def known_data_mock():
 
 def test_default_fitness(executor_mock, trace_mock, known_data_mock):
     ff = BranchDistanceSuiteFitnessFunction(executor_mock)
-    assert ff._compute_fitness(False, trace_mock, known_data_mock) == 0
+    assert ff._compute_fitness(trace_mock, known_data_mock) == 0
 
 
 def test_fitness_function_diff(executor_mock, trace_mock, known_data_mock):
@@ -53,7 +53,7 @@ def test_fitness_function_diff(executor_mock, trace_mock, known_data_mock):
     known_data_mock.existing_code_objects.add(1)
     known_data_mock.existing_code_objects.add(2)
     trace_mock.covered_code_objects.add(0)
-    assert ff._compute_fitness(False, trace_mock, known_data_mock) == 2.0
+    assert ff._compute_fitness(trace_mock, known_data_mock) == 2.0
 
 
 def test_fitness_covered(executor_mock, trace_mock, known_data_mock):
@@ -62,13 +62,13 @@ def test_fitness_covered(executor_mock, trace_mock, known_data_mock):
     trace_mock.covered_predicates[0] = 1
     trace_mock.false_distances[0] = 1
     trace_mock.true_distances[0] = 0
-    assert ff._compute_fitness(False, trace_mock, known_data_mock) == 1.0
+    assert ff._compute_fitness(trace_mock, known_data_mock) == 1.0
 
 
 def test_fitness_neither_covered(executor_mock, trace_mock, known_data_mock):
     ff = BranchDistanceSuiteFitnessFunction(executor_mock)
     known_data_mock.existing_predicates.add(0)
-    assert ff._compute_fitness(False, trace_mock, known_data_mock) == 2.0
+    assert ff._compute_fitness(trace_mock, known_data_mock) == 2.0
 
 
 def test_fitness_covered_twice(executor_mock, trace_mock, known_data_mock):
@@ -77,7 +77,7 @@ def test_fitness_covered_twice(executor_mock, trace_mock, known_data_mock):
     trace_mock.covered_predicates[0] = 2
     trace_mock.false_distances[0] = 1
     trace_mock.true_distances[0] = 0
-    assert ff._compute_fitness(False, trace_mock, known_data_mock) == 0.5
+    assert ff._compute_fitness(trace_mock, known_data_mock) == 0.5
 
 
 def test_fitness_covered_both(executor_mock, trace_mock, known_data_mock):
@@ -86,7 +86,7 @@ def test_fitness_covered_both(executor_mock, trace_mock, known_data_mock):
     trace_mock.covered_predicates[0] = 2
     trace_mock.false_distances[0] = 0
     trace_mock.true_distances[0] = 0
-    assert ff._compute_fitness(False, trace_mock, known_data_mock) == 0.0
+    assert ff._compute_fitness(trace_mock, known_data_mock) == 0.0
 
 
 def test_fitness_normalized(executor_mock, trace_mock, known_data_mock):
@@ -95,19 +95,12 @@ def test_fitness_normalized(executor_mock, trace_mock, known_data_mock):
     trace_mock.covered_predicates[0] = 2
     trace_mock.false_distances[0] = 0
     trace_mock.true_distances[0] = 7.0
-    assert ff._compute_fitness(False, trace_mock, known_data_mock) == 0.875
+    assert ff._compute_fitness(trace_mock, known_data_mock) == 0.875
 
 
 def test_is_maximisation_function(executor_mock):
     ff = BranchDistanceSuiteFitnessFunction(executor_mock)
     assert not ff.is_maximisation_function()
-
-
-def test_has_exception(executor_mock, trace_mock, known_data_mock):
-    ff = BranchDistanceSuiteFitnessFunction(executor_mock)
-    known_data_mock.existing_predicates.add(0)
-    known_data_mock.existing_code_objects.add(0)
-    assert ff._compute_fitness(True, trace_mock, known_data_mock) == 3.0
 
 
 @pytest.mark.parametrize("has_ex", [pytest.param(True), pytest.param(False)])

commit 4170c893627c2469a2e6d472cb4efcf0e6e25426

Generator: fix regression in statistics tracking
Commit d7e83f7 introduced a regression: the basic statistics on size and
length of the test suite were tracked for the combined and the failing
suite, instead of the non-failing and failing suite.  Otherwise, one
needs to subtract the failing size and length from the non-failing
numbers during analysis, to achieve the correct results.

pynguin/generator.py
@@ -219,7 +219,7 @@ class Pynguin:
                     failing.test_chromosomes, "_failing", wrap_code=True
                 )
 
-        self._track_statistics(combined, failing)
+        self._track_statistics(non_failing, failing)
         self._collect_statistics()
         if not StatisticsTracker().write_statistics():
             self._logger.error("Failed to write statistics data")
@@ -261,13 +261,13 @@ class Pynguin:
 
     @staticmethod
     def _track_statistics(
-        combined: tsc.TestSuiteChromosome, failing: tsc.TestSuiteChromosome,
+        non_failing: tsc.TestSuiteChromosome, failing: tsc.TestSuiteChromosome,
     ) -> None:
         tracker = StatisticsTracker()
-        tracker.current_individual(combined)
-        tracker.track_output_variable(RuntimeVariable.Size, combined.size())
+        tracker.current_individual(non_failing)
+        tracker.track_output_variable(RuntimeVariable.Size, non_failing.size())
         tracker.track_output_variable(
-            RuntimeVariable.Length, combined.total_length_of_test_cases
+            RuntimeVariable.Length, non_failing.total_length_of_test_cases
         )
         tracker.track_output_variable(RuntimeVariable.FailingSize, failing.size())
         tracker.track_output_variable(