8322230af9de683fee4d709bf864cb9db72c1ca2
Stephan Lukasczyk <stephan@lukasczyk.me>
2020-08-04 09:17:14 +0200

    Sphinx: second attempt to fix

--- docs/conf.py
+++ docs/conf.py
@@ -4,9 +4,12 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 
+import pynguin
+
 project = "pynguin"
 author = "Pynguin Contributors"
 copyright = f"2020, {author}"
+version = pynguin.__version__
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
