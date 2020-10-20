e8c114fe64883fa0792dd5404b7aa1337a17bdc0
Florian Kroiss <kroiss@fim.uni-passau.de>
2020-10-20 15:34:57 +0200

    Add tests for primitive and none assertion

---
+++ tests/assertion/test_primitiveassertion.py
@@ -0,0 +1,28 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2020 Pynguin Contributors
+#
+#  SPDX-License-Identifier: LGPL-3.0-or-later
+#
+from unittest.mock import MagicMock
+
+import pynguin.assertion.primitiveassertion as pa
+
+
+def test_accept():
+    visitor = MagicMock()
+    assertion = pa.PrimitiveAssertion(MagicMock(), 1337)
+    assertion.accept(visitor)
+    visitor.visit_primitive_assertion.assert_called_with(assertion)
+
+
+def test_clone():
+    source = MagicMock()
+    cloned_ref = MagicMock()
+    source.clone.return_value = cloned_ref
+    assertion = pa.PrimitiveAssertion(source, 1337)
+    new_test_case = MagicMock()
+    cloned = assertion.clone(new_test_case, 20)
+    source.clone.assert_called_with(new_test_case, 20)
+    assert cloned.source == cloned_ref
+    assert cloned.value == 1337
