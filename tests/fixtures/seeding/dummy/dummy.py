3b80080edc041d806cffc9c6f193a1bb563499fe
Stephan Lukasczyk <stephan@lukasczyk.me>
2020-11-03 09:53:50 +0100

    Add docstrings as suggested in code review

--- tests/fixtures/seeding/dummy/dummy.py
+++ tests/fixtures/seeding/dummy/dummy.py
@@ -4,21 +4,19 @@
 #
 #  SPDX-License-Identifier: LGPL-3.0-or-later
 #
-
-#  This file is part of Pynguin.
-#
-#
-#  SPDX-License-Identifier: LGPL-3.0-or-later
-#
+"""A dummy module to be used as a test fixture."""
 
 
 def dummy():
+    """A dummy method in the test fixture."""
     foo = "bar"  # noqa
     bar = 23  # noqa
     baz = 23.42  # noqa
 
 
 class Dummy:
+    """A dummy class in the test fixture."""
+
     _foo = "foo"
     _bar = 42
     _baz = 42.23
