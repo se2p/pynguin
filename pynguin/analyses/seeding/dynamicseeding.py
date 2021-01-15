98c9d6e556c895a55b3dffd459bae1003cc53385
Lukas Steffens <steffens@fim.uni-passau.de>
2021-01-15 11:47:26 +0100

    Fixed some bugs and extended seeding of string methods.

--- pynguin/analyses/seeding/dynamicseeding.py
+++ pynguin/analyses/seeding/dynamicseeding.py
@@ -93,114 +93,84 @@ class DynamicSeeding:
         else:
             pass
 
-    def add_value_for_isalnum(self, value: str):
+    def add_value_for_strings(self, value: str, name: str):
+        method_name = "_add_value_for_" + name
+        method = getattr(self, method_name)
+        method(value)
+
+    def _add_value_for_isalnum(self, value: str):
         if not isinstance(value, str):
             return
         self._dynamic_pool["string"].add(value)
         if value.isalnum():
             self._dynamic_pool["string"].add(value + "!")
         else:
-            delchars = ''.join(c for c in map(chr, range(256)) if not c.isalnum())
-            value_as_alnum = value.translate({delchars: None})
-            if value_as_alnum is not None:
-                self._dynamic_pool["String"].add(value_as_alnum)
-            else:
-                self._dynamic_pool["String"].add("is_alnum")
+            self._dynamic_pool["string"].add("isalnum")
 
-    def add_value_for_islower(self, value: str):
+    def _add_value_for_islower(self, value: str):
         self._dynamic_pool["string"].add(value)
         if value.islower():
             self._dynamic_pool["string"].add(value.upper())
         else:
             self._dynamic_pool["string"].add(value.lower())
 
-    def add_value_for_isupper(self, value: str):
+    def _add_value_for_isupper(self, value: str):
         self._dynamic_pool["string"].add(value)
         if value.isupper():
             self._dynamic_pool["string"].add(value.lower())
         else:
             self._dynamic_pool["string"].add(value.upper())
 
-    def add_value_for_isdecimal(self, value: str):
+    def _add_value_for_isdecimal(self, value: str):
         self._dynamic_pool["string"].add(value)
         if value.isdecimal():
             self._dynamic_pool["string"].add("non_decimal")
         else:
-            delchars = ''.join(c for c in map(chr, range(256)) if not c.isdecimal())
-            value_as_decimal = value.translate({delchars: None})
-            if value_as_decimal is not None:
-                self._dynamic_pool["string"].add(value_as_decimal)
-            else:
-                self._dynamic_pool["string"].add("0123456789")
+            self._dynamic_pool["string"].add("0123456789")
 
-    def add_value_for_isalpha(self, value: str):
+    def _add_value_for_isalpha(self, value: str):
         self._dynamic_pool["string"].add(value)
         if value.isalpha():
             self._dynamic_pool["string"].add(value + "1")
         else:
-            delchars = ''.join(c for c in map(chr, range(256)) if not c.isalpha())
-            value_as_alpha = value.translate({delchars: None})
-            if value_as_alpha is not None:
-                self._dynamic_pool["string"].add(value_as_alpha)
-            else:
-                self._dynamic_pool["string"].add("isalpha")
+            self._dynamic_pool["string"].add("isalpha")
 
-    def add_value_for_isdigit(self, value: str):
+    def _add_value_for_isdigit(self, value: str):
         self._dynamic_pool["string"].add(value)
         if value.isdigit():
             self._dynamic_pool["string"].add(value + "_")
         else:
-            delchars = ''.join(c for c in map(chr, range(256)) if not c.isdigit())
-            value_as_digit = value.translate({delchars: None})
-            if value_as_digit is not None:
-                self._dynamic_pool["string"].add(value_as_digit)
-            else:
-                self._dynamic_pool["string"].add("0")
+            self._dynamic_pool["string"].add("0")
 
-    def add_value_for_isidentifier(self, value: str):
+    def _add_value_for_isidentifier(self, value: str):
         self._dynamic_pool["string"].add(value)
         if value.isidentifier():
             self._dynamic_pool["string"].add(value + "!")
         else:
-            delchars = ''.join(c for c in map(chr, range(256)) if not c.isidentifier())
-            value_as_identifier = value.translate({delchars: None})
-            if value_as_identifier is not None:
-                self._dynamic_pool["string"].add(value_as_identifier)
-            else:
-                self._dynamic_pool["string"].add("is_Identifier")
+            self._dynamic_pool["string"].add("is_Identifier")
 
-    def add_value_for_isnumeric(self, value: str):
+    def _add_value_for_isnumeric(self, value: str):
         self._dynamic_pool["string"].add(value)
         if value.isnumeric():
             self._dynamic_pool["string"].add(value + "A")
         else:
-            delchars = ''.join(c for c in map(chr, range(256)) if not c.isnumeric())
-            value_as_numeric = value.translate({delchars: None})
-            if value_as_numeric is not None:
-                self._dynamic_pool["string"].add(value_as_numeric)
-            else:
-                self._dynamic_pool["string"].add("012345")
+            self._dynamic_pool["string"].add("012345")
 
-    def add_value_for_isprintable(self, value: str):
+    def _add_value_for_isprintable(self, value: str):
         self._dynamic_pool["string"].add(value)
         if value.isprintable():
             self._dynamic_pool["string"].add(value + "\n")
         else:
-            delchars = ''.join(c for c in map(chr, range(256)) if not c.isprintable())
-            value_as_printable = value.translate({delchars: None})
-            if value_as_printable is not None:
-                self._dynamic_pool["string"].add(value_as_printable)
-            else:
-                self._dynamic_pool["string"].add("is_printable")
+            self._dynamic_pool["string"].add("is_printable")
 
-    def add_value_for_isspace(self, value: str):
+    def _add_value_for_isspace(self, value: str):
         self._dynamic_pool["string"].add(value)
         if value.isspace():
             self._dynamic_pool["string"].add(value + "a")
         else:
             self._dynamic_pool["string"].add("   ")
 
-    def add_value_for_istitle(self, value: str):
+    def _add_value_for_istitle(self, value: str):
         self._dynamic_pool["string"].add(value)
         if value.istitle():
             self._dynamic_pool["string"].add(value + " AAA")
