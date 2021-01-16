4369c1fbcc098dee4542cd83af01b3cd4522af67
Lukas Steffens <steffens@fim.uni-passau.de>
2021-01-16 23:35:15 +0100

    Added a few more dummies for testing.

--- tests/fixtures/seeding/dynamicseeding/dynamicseedingdummies.py
+++ tests/fixtures/seeding/dynamicseeding/dynamicseedingdummies.py
@@ -1,5 +1,31 @@
-# Dummy method for testing the str.isalnum() function
-def isalnum_test(s1):
+# Dummy method for testing adding a value
+def compare_op_dummy(x, y):
+    if x == y:
+        return 0
+    else:
+        return 1
+
+
+# Dummy method for testing the str.startswith method
+def startswith_dummy(s1, s2):
+    if s1.startswith(s2):
+        return 0
+    else:
+        return 1
+
+
+# Dummy method for testing the str.endswith method
+def endswith_dummy(s1, s2):
+    if s1.endswith(s2):
+        return 0
+    else:
+        return 1
+
+
+# Dummy method for testing the str.isalnum() method
+def isalnum_dummy(s1):
+    if not isinstance(s1, str):
+        return 2
     if s1.isalnum():
         return 0
     else:
