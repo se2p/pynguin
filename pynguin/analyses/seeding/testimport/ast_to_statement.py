e685c247475fc9b2ef149068bccc1dcadf9425fc
Lukas Steffens <steffens@fim.uni-passau.de>
2021-02-01 14:12:36 +0100

    Adapted structure of the methods creating statements.

--- pynguin/analyses/seeding/testimport/ast_to_statement.py
+++ pynguin/analyses/seeding/testimport/ast_to_statement.py
@@ -5,17 +5,25 @@ import pynguin.testcase.statements.parametrizedstatements as param_stmt
 import pynguin.testcase.statements.primitivestatements as prim_stmt
 import pynguin.testcase.testcase as tc
 import pynguin.testcase.variable.variablereference as vr
+from pynguin.testcase.statements.statement import Statement
 from pynguin.utils.generic.genericaccessibleobject import GenericCallableAccessibleObject
 
 
 class AstToStatement:
     @staticmethod
-    def create_prim_stmt(assign: ast.Assign, testcase: tc.TestCase) -> Tuple[str, prim_stmt.PrimitiveStatement]:
-        """Creates a primitive statement from an ast.Assign node.
+    def create_assign_stmt(
+        assign: ast.Assign,
+        testcase: tc.TestCase,
+        objs_under_test: Set,
+        ref_dict: Dict[str, vr.VariableReference]
+    ) -> Tuple[str, Statement]:
+        """Creates the corresponding statement from an ast.Assign node.
 
         Args:
             assign: The ast.Assign node
             testcase: The testcase of the statement
+            objs_under_test: the accessible objects under test
+            ref_dict: a dictionary containing key value pairs of variable ids and variable references.
 
         Returns:
             The corresponding primitive statement
@@ -25,15 +33,7 @@ class AstToStatement:
         elif type(assign.value) is ast.UnaryOp:
             new_stmt = AstToStatement._create_stmt_from_unaryop(assign, testcase)
         elif type(assign.value) is ast.Call:
-            new_stmt = AstToStatement._create_stmt_from_call(assign, testcase)
-        elif isinstance(assign.value.operand.value, int):
-            new_stmt = prim_stmt.IntPrimitiveStatement(testcase, (-1) * assign.value.operand.value)
-        elif isinstance(assign.value.operand.value, float):
-            new_stmt = prim_stmt.FloatPrimitiveStatement(testcase, (-1) * assign.value.operand.value)
-        elif isinstance(assign.value.operand.value, str):
-            new_stmt = prim_stmt.StringPrimitiveStatement(testcase, assign.value.operand.value)
-        elif isinstance(assign.value.operand.value, bool):
-            new_stmt = prim_stmt.BooleanPrimitiveStatement(testcase, assign.value.operand.value)
+            new_stmt = AstToStatement._create_stmt_from_call(assign, testcase, objs_under_test, ref_dict)
         else:
             raise Exception
 
@@ -52,16 +52,43 @@ class AstToStatement:
         return var_refs
 
     @staticmethod
-    def create_function_stmt(
-        expr: ast.Expr,
+    def _create_stmt_from_constant(assign: ast.Assign, testcase: tc.TestCase):
+        if assign.value.value is None:
+            return prim_stmt.NoneStatement(testcase, assign.value.value)
+
+        val = assign.value.value
+        if isinstance(val, bool):
+            return prim_stmt.BooleanPrimitiveStatement(testcase, assign.value.value)
+        elif isinstance(val, int):
+            return prim_stmt.IntPrimitiveStatement(testcase, assign.value.value)
+        elif isinstance(val, float):
+            return prim_stmt.FloatPrimitiveStatement(testcase, assign.value.value)
+        elif isinstance(val, str):
+            return prim_stmt.StringPrimitiveStatement(testcase, assign.value.value)
+        else:
+            return None
+
+    @staticmethod
+    def _create_stmt_from_unaryop(assign: ast.Assign, testcase: tc.TestCase):
+        val = assign.value.operand.value
+        if isinstance(val, int):
+            return prim_stmt.IntPrimitiveStatement(testcase, (-1) * assign.value.operand.value)
+        elif isinstance(val, float):
+            return prim_stmt.FloatPrimitiveStatement(testcase, (-1) * assign.value.operand.value)
+        elif isinstance(val, bool):
+            return prim_stmt.BooleanPrimitiveStatement(testcase, not assign.value.operand.value)
+
+    @staticmethod
+    def _create_stmt_from_call(
+        assign: ast.Assign,
         testcase: tc.TestCase,
         objs_under_test: Set,
         ref_dict: Dict[str, vr.VariableReference]
     ) -> param_stmt.FunctionStatement:
-        """Creates a function statement from an ast.Expr node.
+        """Creates a function statement from an ast.assign node.
 
         Args:
-            expr: the ast.Expr node
+            assign: the ast.Assign node
             testcase: the testcase of the statement
             objs_under_test: the accessible objects under test
             ref_dict: a dictionary containing key value pairs of variable ids and variable references.
@@ -70,7 +97,7 @@ class AstToStatement:
             The corresponding function statement
         """
         gen_callable = None
-        call = expr.value
+        call = assign.value
         func_name = str(call.func.attr)
         for obj in objs_under_test:
             if func_name == obj.function_name:
@@ -81,19 +108,3 @@ class AstToStatement:
             AstToStatement._create_variable_references_from_call_args(call.args, ref_dict)
         )
         return func_stmt
-
-    @staticmethod
-    def _create_stmt_from_constant(assign: ast.Assign, testcase: tc.TestCase):
-        if assign.value.value is None:
-            return prim_stmt.NoneStatement(testcase, assign.value.value)
-
-        val = assign.value.value
-        if isinstance(val, int):
-            return prim_stmt.IntPrimitiveStatement(testcase, assign.value.value)
-        elif isinstance(val, float):
-            return prim_stmt.FloatPrimitiveStatement(testcase, assign.value.value)
-        elif isinstance(val, str):
-            return prim_stmt.StringPrimitiveStatement(testcase, assign.value.value)
-        else:
-            return None
-
