6bae1f9b5f3334f7740b387ba9881e18325e132b
Stephan Lukasczyk <stephan@lukasczyk.me>
2021-03-03 15:49:24 +0100

    Implement a nominal sub-type analysis
    This analysis utilises Python's method-resolution order to find sub-type
    relationships.  It yields an inheritance graph that contains all found
    sub-type relations.

---
+++ pynguin/analyses/module/inheritance.py
@@ -0,0 +1,180 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2021 Pynguin Contributors
+#
+#  SPDX-License-Identifier: LGPL-3.0-or-later
+"""Provides tools to build an inheritance graph of the subject under test."""
+import inspect
+from typing import Any, NamedTuple, Optional, Set, Type, overload
+
+import networkx as nx
+
+
+class ClassInformation(NamedTuple):
+    """Stores class information in form of fully-qualified name and class object."""
+
+    name: str
+    class_object: type
+
+    def __eq__(self, other: Any) -> bool:
+        if self is other:
+            return True
+        if not isinstance(other, ClassInformation):
+            return False
+        return self.name == other.name
+
+    def __hash__(self) -> int:
+        return hash(self.name)
+
+
+class InheritanceGraph:
+    """Represents the inheritance structure of the found types."""
+
+    def __init__(self) -> None:
+        self._graph = nx.DiGraph()
+
+    def add_edge(self, source: ClassInformation, target: ClassInformation) -> None:
+        """Adds an edge to the graph.
+
+        An edge is a sub-type relation between two classes, as yielded, for example,
+        by querying the method-resolution order.
+
+        Args:
+            source: The super class
+            target: The sub class
+        """
+        self._graph.add_edge(source, target)
+
+    def find(self, class_information: ClassInformation) -> Optional[ClassInformation]:
+        """Finds a class in the inheritance graph.
+
+        Args:
+            class_information: The class to search for
+
+        Returns:
+            The class, if it was found.  None otherwise.
+        """
+        if self._graph.has_node(class_information):
+            for node in self._graph.nodes.keys():
+                if class_information == node:
+                    return node
+        return None
+
+    def get_sub_types(
+        self, class_information: ClassInformation
+    ) -> Set[ClassInformation]:
+        """Provides a set of sub types for a class.
+
+        If there are no sub types, the returned set is empty.
+
+        Args:
+            class_information: The class to start
+
+        Returns:
+            The sub types of the class
+        """
+        if graph_node := self.find(class_information):
+            return self._get_transitive_successors(graph_node, set())
+        raise ValueError("Node for class not found in inheritance graph.")
+
+    def _get_transitive_successors(
+        self, node: ClassInformation, done: Set[ClassInformation]
+    ) -> Set[ClassInformation]:
+        successors: Set[ClassInformation] = set()
+        for successor_node in self._get_successors(node):
+            if successor_node not in done:
+                successors.add(successor_node)
+                done.add(successor_node)
+                successors.update(self._get_transitive_successors(successor_node, done))
+        return successors
+
+    def _get_successors(self, node: ClassInformation) -> Set[ClassInformation]:
+        successors: Set[ClassInformation] = set()
+        for successor in self._graph.successors(node):
+            successors.add(successor)
+        return successors
+
+    def number_of_nodes(self) -> int:
+        """Provides the number of nodes in the graph.
+
+        Returns:
+            The number of nodes in the graph
+        """
+        return self._graph.number_of_nodes()
+
+    def number_of_edges(self) -> int:
+        """Provides the number of edges in the graph.
+
+        Returns:
+            The number of edges in the graph
+        """
+        return self._graph.number_of_edges()
+
+
+def build_inheritance_graph(analysed_classes: Set[Type]) -> InheritanceGraph:
+    """Builds an inheritance graph for a set of types.
+
+    Args:
+        analysed_classes: The set of available types in the program
+
+    Returns:
+        The inheritance graph for these types
+    """
+    graph = InheritanceGraph()
+    for analysed_class in analysed_classes:
+        mro_class_information = [
+            _retrieve_class_information(class_)
+            for class_ in inspect.getmro(analysed_class)
+        ]
+        for target, source in zip(mro_class_information, mro_class_information[1:]):
+            graph.add_edge(source, target)
+    return graph
+
+
+@overload
+def build_class_information(value: ClassInformation) -> ClassInformation:
+    ...  # pragma: no cover
+
+
+@overload
+def build_class_information(value: str) -> ClassInformation:
+    ...  # pragma: no cover
+
+
+@overload
+def build_class_information(value: Type) -> ClassInformation:
+    ...  # pragma: no cover
+
+
+def build_class_information(value) -> ClassInformation:
+    """Builds the class-information object.
+
+    The parameter can either be of type `ClassInformation`, `str`, or `type`.  For
+    other inputs, a `ValueError` is raised.
+
+    Args:
+        value: The value to build the class-information object from
+
+    Returns:
+        A class-information object
+
+    Raises:
+        ValueError: In case no appropriate input was given
+    """
+    if isinstance(value, ClassInformation):
+        return value
+    if isinstance(value, str):
+        return ClassInformation(name=value, class_object=type(None))
+    if isinstance(value, type):
+        return _retrieve_class_information(value)
+    raise ValueError("Cannot build ClassInformation for %s" % value)
+
+
+def _retrieve_class_information(class_object: type) -> ClassInformation:
+    qualified_name = class_object.__qualname__
+    name = class_object.__name__
+    module_name = class_object.__module__
+    if name == qualified_name:
+        qualified_name = f"{module_name}.{name}"
+    class_information = ClassInformation(name=qualified_name, class_object=class_object)
+    return class_information
