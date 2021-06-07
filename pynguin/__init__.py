2b74171f5a372dc93921a5802d2243f3f513ae40
Stephan Lukasczyk <stephan@lukasczyk.me>
2021-06-07 11:39:41 +0200

    Bump version for next development cycle

--- pynguin/__init__.py
+++ pynguin/__init__.py
@@ -17,7 +17,7 @@ StatisticsBackend = config.StatisticsBackend
 StoppingCondition = config.StoppingCondition
 TypeInferenceStrategy = config.TypeInferenceStrategy
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __all__ = [
     "set_configuration",
     "run_pynguin",
