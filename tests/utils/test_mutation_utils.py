68c08cb971e3a68e77d4d4c0cbbd511e72c33568
Florian Kroiss <kroiss@fim.uni-passau.de>
2021-07-12 11:26:54 +0200

    Add some more tests for alpha_exponent_insertion

--- tests/utils/test_mutation_utils.py
+++ tests/utils/test_mutation_utils.py
@@ -5,6 +5,7 @@
 #  SPDX-License-Identifier: LGPL-3.0-or-later
 #
 from unittest import mock
+from unittest.mock import MagicMock
 
 from pynguin.utils.mutation_utils import alpha_exponent_insertion
 
@@ -13,5 +14,19 @@ def test_alpha_exponent_insertion():
     insert = []
     with mock.patch("pynguin.utils.randomness.next_float") as float_mock:
         float_mock.return_value = 0.2
-        alpha_exponent_insertion(insert, lambda: 5)
-        assert insert == [5, 5, 5]
+        with mock.patch("pynguin.utils.randomness.next_int") as int_mock:
+            int_mock.side_effect = 1, 0
+            func = MagicMock()
+            func.side_effect = [0, 1, 2]
+            alpha_exponent_insertion(insert, func)
+            assert insert == [2, 0, 1]
+
+
+def test_alpha_exponent_insertion_none():
+    insert = []
+    with mock.patch("pynguin.utils.randomness.next_float") as float_mock:
+        float_mock.return_value = 0.2
+        func = MagicMock()
+        func.return_value = None
+        alpha_exponent_insertion(insert, func)
+        assert insert == []
