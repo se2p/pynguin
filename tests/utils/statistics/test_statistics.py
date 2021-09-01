120c7f4474bebb00cbb844551f7b8170ce68ac1e
Stephan Lukasczyk <stephan@lukasczyk.me>
2021-09-01 15:53:07 +0200

    Readd test as requested by code review

---
+++ tests/utils/statistics/test_statistics.py
@@ -0,0 +1,17 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2021 Pynguin Contributors
+#
+#  SPDX-License-Identifier: LGPL-3.0-or-later
+#
+import pynguin.utils.statistics.statistics as stat
+from pynguin.utils.statistics.runtimevariable import RuntimeVariable
+
+
+def test_variables_generator():
+    value_1 = 23.42
+    value_2 = 42.23
+    stat.track_output_variable(RuntimeVariable.TotalTime, value_1)
+    stat.track_output_variable(RuntimeVariable.TotalTime, value_2)
+    result = [v for _, v in stat.variables_generator]
+    assert result in ([], [value_1, value_2])
