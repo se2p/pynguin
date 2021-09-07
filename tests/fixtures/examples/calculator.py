7dfade878912028ff4fe2bc7884358dde940631f
Florian Straubinger <straubinge@fim.uni-passau.de>
2021-09-07 17:13:14 +0200

    Added calculator examples

---
+++ tests/fixtures/examples/calculator.py
@@ -0,0 +1,50 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2021 Pynguin Contributors
+#
+#  SPDX-License-Identifier: LGPL-3.0-or-later
+#
+
+class CalculatorResult:
+    def __init__(self, op: str, result: float):
+        self.last_op = op
+        self.last_result = result
+
+    def __repr__(self) -> str:
+        return f'{self.last_op.upper()}: {self.last_result}'
+
+
+class Calculator:
+    amount_calculation = 0
+    results = []
+
+    class Decorators:
+        @staticmethod
+        def calc_decorator(func):
+            def inner(self, a, b):
+                result = func(self, a, b)
+                cr = CalculatorResult(func.__name__, result)
+                Calculator.results.append(cr)
+                Calculator.amount_calculation += 1
+                return result
+
+            return inner
+
+    @Decorators.calc_decorator
+    def add(self, a: float, b: float) -> float:
+        return a + b
+
+    @Decorators.calc_decorator
+    def sub(self, a: float, b: float) -> float:
+        return a - b
+
+    @Decorators.calc_decorator
+    def mult(self, a: float, b: float) -> float:
+        return a * b
+
+    @Decorators.calc_decorator
+    def div(self, a: float, b: float) -> float:
+        return a / b
+
+    def output_results(self) -> str:
+        return "".join([str(x) + '\n' for x in self.results]).rstrip()
---
+++ tests/fixtures/examples/calculator_simple.py
@@ -0,0 +1,37 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2021 Pynguin Contributors
+#
+#  SPDX-License-Identifier: LGPL-3.0-or-later
+#
+
+
+class Calculator:
+    results = []
+
+    def __init__(self):
+        self.amount_calculation = 0
+
+    def add(self, a: float, b: float) -> float:
+        self.amount_calculation += 1
+        ret = a + b
+        Calculator.results.append(ret)
+        return ret
+
+    def sub(self, a: float, b: float) -> float:
+        self.amount_calculation += 1
+        ret = a - b
+        Calculator.results.append(ret)
+        return ret
+
+    def mult(self, a: float, b: float) -> float:
+        self.amount_calculation += 1
+        ret = a * b
+        Calculator.results.append(ret)
+        return ret
+
+    def div(self, a: float, b: float) -> float:
+        self.amount_calculation += 1
+        ret = a / b
+        Calculator.results.append(ret)
+        return ret
