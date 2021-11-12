3c594649763991d7d8d1064503b6af1af9ceab77
Florian Kroiss <kroiss@fim.uni-passau.de>
2021-11-12 18:04:12 +0100

    Do not keep track of generated assertions. Also dedent methods, because they don't need to be part of the class.

--- pynguin/assertion/mutation_analysis/mutationanalysisgenerator.py
+++ pynguin/assertion/mutation_analysis/mutationanalysisgenerator.py
@@ -7,7 +7,7 @@
 """Provides a assertion generation by utilizing the mutation-analysis approach."""
 import logging
 from types import ModuleType
-from typing import Any, List, Set, Tuple
+from typing import Any, List, Tuple
 
 import pynguin.assertion.assertion as ass
 import pynguin.assertion.complexassertion as ca
@@ -40,8 +40,6 @@ class MutationAnalysisGenerator(cv.ChromosomeVisitor):
         self._executor = executor
         self._executor.add_observer(sco.StateCollectingObserver(self._storage))
 
-        self._assertions: Set[ass.Assertion] = set()
-
     def visit_test_suite_chromosome(self, chromosome: tsc.TestSuiteChromosome) -> None:
         test_cases = [chrom.test_case for chrom in chromosome.test_case_chromosomes]
 
@@ -73,121 +71,121 @@ class MutationAnalysisGenerator(cv.ChromosomeVisitor):
             mutated = self._storage.get_mutations(key)
             # Iterate over each mutated item
             for mut_value in mutated:
-                self._compare(key, ref_value, mut_value)
+                _compare(key, ref_value, mut_value)
 
-    def _compare(self, key: Tuple[Any, ...], ref_value: Any, mut_value: Any):
-        if self._is_assertable_item(ref_value):
-            # ref_value and mut_value can be compared straight away
+
+def _compare(key: Tuple[Any, ...], ref_value: Any, mut_value: Any):
+    if _is_assertable_item(ref_value):
+        # ref_value and mut_value can be compared straight away
+        if (
+            ref_value != mut_value
+            or config.configuration.test_case_output.generate_all_assertions
+        ):
+            _switch_generation(key, ref_value)
+    elif hasattr(ref_value, "__dict__"):
+        # ref_value and mut_value need to be disassembled and partially compared
+        for (ref_field, ref_field_value), (mut_field, mut_field_value) in zip(
+            vars(ref_value).items(), vars(mut_value).items()
+        ):
             if (
-                ref_value != mut_value
-                or config.configuration.test_case_output.generate_all_assertions
+                ref_field == mut_field
+                and _is_assertable_item(ref_field_value)
+                and (
+                    ref_field_value != mut_field_value
+                    or config.configuration.test_case_output.generate_all_assertions
+                )
             ):
-                self._switch_generation(key, ref_value)
-        elif hasattr(ref_value, "__dict__"):
-            # ref_value and mut_value need to be disassembled and partially compared
-            for (ref_field, ref_field_value), (mut_field, mut_field_value) in zip(
-                vars(ref_value).items(), vars(mut_value).items()
-            ):
-                if (
-                    ref_field == mut_field
-                    and self._is_assertable_item(ref_field_value)
-                    and (
-                        ref_field_value != mut_field_value
-                        or config.configuration.test_case_output.generate_all_assertions
-                    )
-                ):
-                    self._switch_generation(key, ref_field_value, ref_field)
+                _switch_generation(key, ref_field_value, ref_field)
 
-    def _switch_generation(
-        self, key: Tuple[Any, ...], ref_value: Any, field: str = None
-    ):
-        if key[0] == cs.EntryTypes.RETURN_VALUE:
-            self._gen_return_value_assertion(key, ref_value, field)
-        elif key[0] == cs.EntryTypes.GLOBAL_FIELD:
-            self._gen_global_field_assertion(key, ref_value, field)
-        elif key[0] == cs.EntryTypes.OBJECT_ATTRIBUTE:
-            self._gen_object_attribute_assertion(key, ref_value, field)
-        elif key[0] == cs.EntryTypes.CLASS_FIELD:
-            self._gen_class_variable_assertion(key, ref_value, field)
-        else:
-            raise ValueError("Unknown entry type.")
 
-    def _gen_return_value_assertion(
-        self, key: Tuple[Any, ...], ref_value: Any, field: str = None
-    ):
-        statement = key[1]
-        if field:
-            assertion: ass.Assertion = fa.FieldAssertion(
-                statement.ret_val, ref_value, field
-            )
-        else:
-            assertion = ca.ComplexAssertion(statement.ret_val, ref_value)
-        self._add_assertion(statement, assertion)
+def _switch_generation(key: Tuple[Any, ...], ref_value: Any, field: str = None):
+    if key[0] == cs.EntryTypes.RETURN_VALUE:
+        _gen_return_value_assertion(key, ref_value, field)
+    elif key[0] == cs.EntryTypes.GLOBAL_FIELD:
+        _gen_global_field_assertion(key, ref_value, field)
+    elif key[0] == cs.EntryTypes.OBJECT_ATTRIBUTE:
+        _gen_object_attribute_assertion(key, ref_value, field)
+    elif key[0] == cs.EntryTypes.CLASS_FIELD:
+        _gen_class_variable_assertion(key, ref_value, field)
+    else:
+        raise ValueError("Unknown entry type.")
 
-    def _gen_global_field_assertion(
-        self, key: Tuple[Any, ...], ref_value: Any, field: str = None
-    ):
-        statement = key[1]
-        module_name = key[2]
-        global_field = key[3]
-        if field:
-            assertion = fa.FieldAssertion(
-                None, ref_value, field, module_name, [global_field]
-            )
-        else:
-            assertion = fa.FieldAssertion(None, ref_value, global_field, module_name)
-        self._add_assertion(statement, assertion)
 
-    def _gen_object_attribute_assertion(
-        self, key: Tuple[Any, ...], ref_value: Any, field: str = None
-    ):
-        statement = key[1]
-        obj_vr = key[2]
-        obj_field = key[3]
-        if field:
-            assertion = fa.FieldAssertion(obj_vr, ref_value, field, None, [obj_field])
-        else:
-            assertion = fa.FieldAssertion(obj_vr, ref_value, obj_field)
-        self._add_assertion(statement, assertion)
-
-    def _gen_class_variable_assertion(
-        self, key: Tuple[Any, ...], ref_value: Any, field: str = None
-    ):
-        statement = key[1]
-        clazz = key[2]
-        clazz_field = key[3]
-        clazz_name = clazz.__name__
-        clazz_module = clazz.__module__
-        if field:
-            assertion = fa.FieldAssertion(
-                None,
-                ref_value,
-                field,
-                clazz_module,
-                [clazz_field, clazz_name],
-            )
-        else:
-            assertion = fa.FieldAssertion(
-                None, ref_value, clazz_field, clazz_module, [clazz_name]
-            )
-        self._add_assertion(statement, assertion)
-
-    def _add_assertion(self, statement, assertion):
-        if assertion not in self._assertions:
-            self._assertions.add(assertion)
-            statement.add_assertion(assertion)
-
-    @staticmethod
-    def _is_comparable_object(obj: Any) -> bool:
-        return vars(obj.__class__).get("__eq__", None) and vars(obj.__class__).get(
-            "__hash__", None
+def _gen_return_value_assertion(
+    key: Tuple[Any, ...], ref_value: Any, field: str = None
+):
+    statement = key[1]
+    if field:
+        assertion: ass.Assertion = fa.FieldAssertion(
+            statement.ret_val, ref_value, field
         )
+    else:
+        assertion = ca.ComplexAssertion(statement.ret_val, ref_value)
+    statement.add_assertion(assertion)
 
-    def _is_assertable_item(self, item: Any) -> bool:
-        return (
-            tu.is_primitive_type(type(item))
-            or tu.is_none_type(type(item))
-            or tu.is_enum(type(item))
-            or tu.is_collection_type(type(item))
-            or self._is_comparable_object(item)
+
+def _gen_global_field_assertion(
+    key: Tuple[Any, ...], ref_value: Any, field: str = None
+):
+    statement = key[1]
+    module_name = key[2]
+    global_field = key[3]
+    if field:
+        assertion = fa.FieldAssertion(
+            None, ref_value, field, module_name, [global_field]
         )
+    else:
+        assertion = fa.FieldAssertion(None, ref_value, global_field, module_name)
+    statement.add_assertion(assertion)
+
+
+def _gen_object_attribute_assertion(
+    key: Tuple[Any, ...], ref_value: Any, field: str = None
+):
+    statement = key[1]
+    obj_vr = key[2]
+    obj_field = key[3]
+    if field:
+        assertion = fa.FieldAssertion(obj_vr, ref_value, field, None, [obj_field])
+    else:
+        assertion = fa.FieldAssertion(obj_vr, ref_value, obj_field)
+    statement.add_assertion(assertion)
+
+
+def _gen_class_variable_assertion(
+    key: Tuple[Any, ...], ref_value: Any, field: str = None
+):
+    statement = key[1]
+    clazz = key[2]
+    clazz_field = key[3]
+    clazz_name = clazz.__name__
+    clazz_module = clazz.__module__
+    if field:
+        assertion = fa.FieldAssertion(
+            None,
+            ref_value,
+            field,
+            clazz_module,
+            [clazz_field, clazz_name],
+        )
+    else:
+        assertion = fa.FieldAssertion(
+            None, ref_value, clazz_field, clazz_module, [clazz_name]
+        )
+    statement.add_assertion(assertion)
+
+
+def _is_comparable_object(obj: Any) -> bool:
+    return vars(obj.__class__).get("__eq__", None) and vars(obj.__class__).get(
+        "__hash__", None
+    )
+
+
+def _is_assertable_item(item: Any) -> bool:
+    return (
+        tu.is_primitive_type(type(item))
+        or tu.is_none_type(type(item))
+        or tu.is_enum(type(item))
+        or tu.is_collection_type(type(item))
+        or _is_comparable_object(item)
+    )
