f1865b2a6b08b562bf33224bdfb1403c7c4ea031
Stephan Lukasczyk <stephan@lukasczyk.me>
2022-03-17 12:55:52 +0100

    Add ability to (de)select type information in AST

--- pynguin/analyses/module.py
+++ pynguin/analyses/module.py
@@ -11,7 +11,7 @@ import inspect
 import logging
 import sys
 from types import ModuleType
-from typing import NamedTuple
+from typing import Any, NamedTuple
 
 LOGGER = logging.getLogger(__name__)
 
@@ -22,9 +22,19 @@ class _ParseResult(NamedTuple):
     module_name: str
     module: ModuleType
     syntax_tree: ast.AST | None
+    contains_type_information: bool
 
 
-def parse_module(module_name: str) -> _ParseResult:
+class _ArgumentAnnotationRemovalVisitor(ast.NodeTransformer):
+    """Removes argument annotations from an AST."""
+
+    # pylint: disable=missing-function-docstring, no-self-use
+    def visit_arg(self, node: ast.arg) -> Any:
+        node.annotation = None
+        return node
+
+
+def parse_module(module_name: str, extract_types: bool = True) -> _ParseResult:
     """Parses a module and extracts its module-type and AST.
 
     If the source code is not available it is not possible to build an AST.  In this
@@ -34,6 +44,7 @@ def parse_module(module_name: str) -> _ParseResult:
 
     Args:
         module_name: The fully-qualified name of the module
+        extract_types: Whether to extract type information into the AST
 
     Returns:
         A tuple of the imported module type and its optional AST
@@ -44,12 +55,24 @@ def parse_module(module_name: str) -> _ParseResult:
         syntax_tree = ast.parse(
             inspect.getsource(module),
             filename=module_name.split(".")[-1] + ".py",
-            type_comments=True,
-            feature_version=sys.version_info[:2],
+            type_comments=extract_types,
+            feature_version=sys.version_info[1],
         )
+        if not extract_types:
+            # The parameter type_comments of the AST library's parse function does not
+            # prevent that the annotation is present in the AST.  Thus, we explicitly
+            # remove it if we do not want the types to be extracted.
+            # This is a hack, maybe I do not understand how to use ast.parse properly...
+            annotation_remover = _ArgumentAnnotationRemovalVisitor()
+            annotation_remover.visit(syntax_tree)
     except OSError as error:
         LOGGER.warning(
             f"Could not retrieve source code for module {module_name} ({error})."
         )
         syntax_tree = None
-    return _ParseResult(module_name=module_name, module=module, syntax_tree=syntax_tree)
+    return _ParseResult(
+        module_name=module_name,
+        module=module,
+        syntax_tree=syntax_tree,
+        contains_type_information=extract_types,
+    )
--- tests/analyses/test_module.py
+++ tests/analyses/test_module.py
@@ -13,6 +13,7 @@ def test_parse_module():
     assert parse_result.module.__name__ == module_name
     assert parse_result.module_name == module_name
     assert parse_result.syntax_tree is not None
+    assert parse_result.contains_type_information
 
 
 def test_parse_c_module():
@@ -21,3 +22,20 @@ def test_parse_c_module():
     assert parse_result.module.__name__ == module_name
     assert parse_result.module_name == module_name
     assert parse_result.syntax_tree is None
+    assert parse_result.contains_type_information
+
+
+def test_parse_module_check_for_type_hint():
+    module_name = "tests.fixtures.cluster.no_dependencies"
+    parse_result = parse_module(module_name)
+    annotated_type = parse_result.syntax_tree.body[1].args.args[0].annotation.id
+    assert annotated_type == "float"
+    assert parse_result.contains_type_information
+
+
+def test_parse_module_check_for_no_type_hint():
+    module_name = "tests.fixtures.cluster.no_dependencies"
+    parse_result = parse_module(module_name, extract_types=False)
+    annotated_type = parse_result.syntax_tree.body[1].args.args[0].annotation
+    assert annotated_type is None
+    assert not parse_result.contains_type_information
