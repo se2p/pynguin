c704fe998c3fae520cd7352033e7645af016d11c
Stephan Lukasczyk <stephan@lukasczyk.me>
2022-05-24 16:04:04 +0200

    Bump version numbers

--- pynguin/__init__.py
+++ pynguin/__init__.py
@@ -16,7 +16,7 @@ ExportStrategy = config.ExportStrategy
 StatisticsBackend = config.StatisticsBackend
 TypeInferenceStrategy = config.TypeInferenceStrategy
 
-__version__ = "0.20.0"
+__version__ = "0.21.0.dev"
 __all__ = [
     "set_configuration",
     "run_pynguin",
