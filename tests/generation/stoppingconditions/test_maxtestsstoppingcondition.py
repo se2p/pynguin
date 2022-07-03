6ff02457c544a07f35ebc9296afc9aece124d18d
Florian Kroiss <kroiss@fim.uni-passau.de>
2022-07-03 13:05:29 +0200

    Use thread-local storage to eliminate race conditions

--- tests/generation/stoppingconditions/test_maxtestsstoppingcondition.py
+++ tests/generation/stoppingconditions/test_maxtestsstoppingcondition.py
@@ -28,6 +28,6 @@ def test_is_not_fulfilled(stopping_condition):
 
 def test_is_fulfilled(stopping_condition):
     stopping_condition.set_limit(1)
-    stopping_condition.after_test_case_execution(None, None)
-    stopping_condition.after_test_case_execution(None, None)
+    stopping_condition.before_test_case_execution(None)
+    stopping_condition.before_test_case_execution(None)
     assert stopping_condition.is_fulfilled()
