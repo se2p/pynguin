4da26cdd4ca6b4b61ebbf2b7780c6cce54379548
Florian Kroiss <kroiss@fim.uni-passau.de>
2022-08-24 15:41:50 +0200

    Add some test for typetracing and return type observation

---
+++ tests/fixtures/type_tracing/guess_params.py
@@ -0,0 +1,11 @@
+#  This file is part of Pynguin.
+#
+#  SPDX-FileCopyrightText: 2019–2022 Pynguin Contributors
+#
+#  SPDX-License-Identifier: LGPL-3.0-or-later
+#
+def foo(a, b, c):
+    assert isinstance(a, int)
+    print(3 * a)
+    print(4 + b)
+    print(c / 5)
