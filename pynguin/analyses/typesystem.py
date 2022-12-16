7f50b1d84eb01e4008ffa7ab22f8f3d77a6078c5
Florian Kroiss <kroiss@fim.uni-passau.de>
2022-12-16 18:35:13 +0100

    Disable warnings

--- pynguin/analyses/typesystem.py
+++ pynguin/analyses/typesystem.py
@@ -1603,7 +1603,7 @@ class TypeSystem:  # pylint:disable=too-many-public-methods
         """
         glob: dict[str, Any] = {}
         # Make sure typing constructs are available
-        exec("from typing import *", glob)  # pylint:disable=exec-used
+        exec("from typing import *", glob)  # pylint:disable=exec-used # nosec
         # Make globals from module available
         glob.update(globs)
         # Import any prefixes
@@ -1615,7 +1615,7 @@ class TypeSystem:  # pylint:disable=too-many-public-methods
             potential_import = potential_type.group(0).rpartition(".")[0]
             _LOGGER.info("Try to import %s", potential_import)
             try:
-                exec("import " + potential_import, glob)  # pylint:disable=exec-used
+                exec("import " + potential_import, glob)  # pylint:disable=exec-used # nosec
             except Exception:  # pylint:disable=broad-except
                 # Well...
                 pass
