7578a3bf5d64ec56f1e9cd4926a9610e33f241cd
Kucki2018 <43949430+Kucki2018@users.noreply.github.com>
2023-08-04 20:23:04 +0200

    Use the new stoppingcondition
    No more duplicate code. This refers to the new stoppingcondition

--- src/pynguin/ga/generationalgorithmfactory.py
+++ src/pynguin/ga/generationalgorithmfactory.py
@@ -51,6 +51,7 @@ from pynguin.ga.operators.ranking import RankBasedPreferenceSorting
 from pynguin.ga.operators.selection import RankSelection
 from pynguin.ga.operators.selection import SelectionFunction
 from pynguin.ga.operators.selection import TournamentSelection
+from pynguin.ga.stoppingcondition import MinimumCoverageStoppingCondition
 from pynguin.ga.stoppingcondition import MaxIterationsStoppingCondition
 from pynguin.ga.stoppingcondition import MaxSearchTimeStoppingCondition
 from pynguin.ga.stoppingcondition import MaxStatementExecutionsStoppingCondition
@@ -95,6 +96,9 @@ class GenerationAlgorithmFactory(Generic[C], metaclass=ABCMeta):
             conditions.append(MaxTestExecutionsStoppingCondition(max_test_exec))
         if (max_search_time := stopping.maximum_search_time) >= 0:
             conditions.append(MaxSearchTimeStoppingCondition(max_search_time))
+        if (min_coverage := stopping.minimum_coverage_quick) < 1.0:
+            min_iterations = stopping.minimum_iteration_quick
+            conditions.append(MinimumCoverageStoppingCondition(min_coverage, min_iterations))
         if len(conditions) == 0:
             self._logger.info("No stopping condition configured!")
             self._logger.info(
