655c1a779c9ae133a49fc9743e587a86b5974797
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-03-21 00:07:30 +0100

    Refactor ParentNodeTransformer to be more performant and explicit

--- src/pynguin/assertion/mutation_analysis/utils.py
+++ src/pynguin/assertion/mutation_analysis/utils.py
@@ -41,26 +41,39 @@ class ParentNodeTransformer(ast.NodeTransformer):
         self.parent: ast.AST | None = None
 
     def visit(self, node: ast.AST) -> ast.AST:
-        if getattr(node, "parent", None) is not None:
+        # Copy the node because an optimisation of the AST makes it
+        # reuse the same node at multiple places in the tree to
+        # improve memory usage. It would break our goal to create a
+        # tree with a single parent for each node if we don't copy.
+        if hasattr(node, "parent"):
             node = copy.copy(node)
             if hasattr(node, "lineno"):
                 delattr(node, "lineno")
 
         setattr(node, "parent", self.parent)
-        setattr(node, "children", [])
+        setattr(node, "children", set())
 
+        parent_save = self.parent
         self.parent = node
 
-        result_node = super().visit(node)
+        # Visit the children of the node and discard the result
+        # as it returns the same node with the children modified.
+        super().visit(node)
 
-        self.parent = getattr(node, "parent", None)
+        self.parent = parent_save
 
+        # Add all the ancestors of the node to the children list
+        # of the parent if it exists. This is done here so that
+        # the tree has been fully traversed before adding the children.
         if self.parent is not None:
-            node_children = getattr(node, "children", [])
-            parent_children = getattr(self.parent, "children", [])
-            setattr(self.parent, "children", parent_children + [node] + node_children)
+            parent_children: set[ast.AST] = getattr(self.parent, "children")
 
-        return result_node
+            parent_children.add(node)
+
+            node_children: set[ast.AST] = getattr(node, "children")
+            parent_children.update(node_children)
+
+        return node
 
 
 def create_ast(code: str) -> ast.AST:
