693038387251536bd370b5cbec7595e4c198588b
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2024-12-05 18:27:58 +0100

    Catch exceptions when creating reference bindings

--- src/pynguin/testcase/execution.py
+++ src/pynguin/testcase/execution.py
@@ -1598,12 +1598,20 @@ class SubprocessTestCaseExecutor(TestCaseExecutor):
 
         SubprocessTestCaseExecutor._fix_result_for_pickle(result)
 
-        new_reference_bindings = (
-            reference_bindings
-            if result.assertion_trace.trace
-            and not dill.detect.baditems(reference_bindings)
-            else None
-        )
+        try:
+            new_reference_bindings = (
+                reference_bindings
+                if result.assertion_trace.trace
+                and not dill.detect.baditems(reference_bindings)
+                else None
+            )
+        except Exception as exception:
+            new_reference_bindings = None
+            _LOGGER.warning(
+                "Failed to fix reference bindings for pickle, final results might"
+                " differ from classic execution with same seed: %s",
+                exception,
+            )
 
         sending_connection.send(
             (
@@ -1647,17 +1655,25 @@ class SubprocessTestCaseExecutor(TestCaseExecutor):
         for result in results:
             SubprocessTestCaseExecutor._fix_result_for_pickle(result)
 
-        new_references_bindings = tuple(
-            (
-                reference_bindings
-                if result.assertion_trace.trace
-                and not dill.detect.baditems(reference_bindings)
-                else None
+        try:
+            new_references_bindings = tuple(
+                (
+                    reference_bindings
+                    if result.assertion_trace.trace
+                    and not dill.detect.baditems(reference_bindings)
+                    else None
+                )
+                for result, reference_bindings in zip(
+                    results, references_bindings, strict=True
+                )
             )
-            for result, reference_bindings in zip(
-                results, references_bindings, strict=True
+        except Exception as exception:
+            new_references_bindings = tuple(None for _ in references_bindings)
+            _LOGGER.warning(
+                "Failed to fix reference bindings for pickle, final results might"
+                " differ from classic execution with same seed: %s",
+                exception,
             )
-        )
 
         sending_connection.send(
             (
