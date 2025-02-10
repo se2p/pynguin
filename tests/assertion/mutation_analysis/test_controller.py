d1b4d50ee29b8c11f2d2e1d2635683130bdc9a3f
lukas <lukas.krodinger@gmail.com>
2025-02-10 10:36:36 +0100

    Adding tests for not instrumented MutationController

--- tests/assertion/mutation_analysis/test_controller.py
+++ tests/assertion/mutation_analysis/test_controller.py
@@ -4,22 +4,18 @@
 #
 #  SPDX-License-Identifier: MIT
 #
-import importlib
-import inspect
 import threading
-
-from pathlib import Path
 
 import pytest
 
 import pynguin.assertion.assertiongenerator as ag
+import pynguin.assertion.mutation_analysis.controller as ct
 import pynguin.assertion.mutation_analysis.mutators as mu
 import pynguin.assertion.mutation_analysis.operators as mo
 import pynguin.configuration as config
-
 from pynguin.assertion.mutation_analysis.transformer import ParentNodeTransformer
 from pynguin.testcase.execution import ExecutionTracer
-from tests.testutils import module_to_path
+from tests.testutils import import_module_safe
 
 
 @pytest.mark.parametrize(
@@ -36,17 +32,36 @@ def test_create_mutants(module_name, expected_mutants):
         *mo.experimental_operators,
     ])
 
-    # Attempt to import module dynamically
-    try:
-        module = importlib.import_module(module_name)
-        module_source_code = inspect.getsource(module)
-    except SystemExit:
-        # Handle modules that shouldn't execute on import
-        spec = importlib.util.find_spec(module_name)
-        assert spec is not None, f"Module {module_name} not found."
-        module = importlib.util.module_from_spec(spec)
-        file_name = module_to_path(module_name)
-        module_source_code = Path(file_name).read_text(encoding="utf-8")
+    module, module_source_code = import_module_safe(module_name)
+
+    module_ast = ParentNodeTransformer.create_ast(module_source_code)
+    mutation_controller = ct.MutationController(
+        mutant_generator, module_ast, module
+    )
+    config.configuration.seeding.seed = 42
+
+    mutations = tuple(mutation_controller.create_mutants())
+    mutant_count = mutation_controller.mutant_count()
+
+    assert len(mutations) == expected_mutants
+    assert mutant_count == expected_mutants
+
+
+@pytest.mark.parametrize(
+    "module_name, expected_mutants",
+    [
+        ("tests.fixtures.examples.triangle", 14),
+        ("tests.fixtures.regression.argparse_sys_exit", 6),
+        ("tests.fixtures.regression.not_main", 7),
+    ],
+)
+def test_create_mutants_instrumented(module_name, expected_mutants):
+    mutant_generator = mu.FirstOrderMutator([
+        *mo.standard_operators,
+        *mo.experimental_operators,
+    ])
+
+    module, module_source_code = import_module_safe(module_name)
 
     module_ast = ParentNodeTransformer.create_ast(module_source_code)
     mutation_tracer = ExecutionTracer()
@@ -61,3 +76,4 @@ def test_create_mutants(module_name, expected_mutants):
 
     assert len(mutations) == expected_mutants
     assert mutant_count == expected_mutants
+
