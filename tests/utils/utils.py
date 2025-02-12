6b1920d2f94110c91fb626f50aa8354d5bc743b0
lukas <lukas.krodinger@gmail.com>
2025-02-12 16:47:41 +0100

    Adding comment for print_dunder method

--- tests/utils/utils.py
+++ tests/utils/utils.py
@@ -9,6 +9,12 @@
 def print_dunder(obj):
     """Prints all dunder (__) attributes of an object row by row.
 
+    DISCLAIMER: This function is only used for debugging purposes. Do not use it in
+    production code as it uses the print function.
+
+    Usage: Set a breakpoint somewhere in the code, execute the debugger and once it
+    stops at the breakpoint, switch over to the console and type print_dunder(my_obj).
+
     :param obj: The object to inspect.
     """
     for attr in dir(obj):
