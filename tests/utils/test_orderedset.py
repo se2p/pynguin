3819ce83ecb6d699a8a700a278abccd0d1b974dd
lukas <lukas.krodinger@gmail.com>
2025-02-12 16:01:03 +0100

    Adding more tests for OrderedTypeSet

--- tests/utils/test_orderedset.py
+++ tests/utils/test_orderedset.py
@@ -64,6 +64,58 @@ def test_ordereset_and_intersection(first, second, result):
     assert OrderedSet(first).intersection(OrderedSet(second)) == OrderedSet(result)
 
 
+@pytest.mark.parametrize("length, iterable", [(0, []), (3, [int, float, str]), (2, [int, float, int])])
+def test_orderedtypeset_len(length, iterable):
+    assert len(OrderedTypeSet(iterable)) == length
+
+
+def test_orderedtypeset_copy():
+    ordered = OrderedTypeSet([int, float, str])
+    copied = copy.copy(ordered)
+    assert ordered == copied
+
+
+@pytest.mark.parametrize("element, result", [(bool, False), (str, True)])
+def test_orderedtypeset_contains(element, result):
+    assert (element in OrderedTypeSet([int, float, str])) == result
+
+
+def test_orderedtypeset_reversed():
+    ordered = OrderedTypeSet([int, float, str])
+    assert tuple(reversed(ordered)) == (str, float, int)
+
+
+@pytest.mark.parametrize(
+    "first, second, result",
+    [
+        ([int, float, str], [int, float, str], True),
+        ([int, float, str], [int, float], False),
+        ([int, float, None], [int, float], False),
+        ([int, float, str], [str, float, int], False),
+    ],
+)
+def test_orderedtypeset_eq(first, second, result):
+    assert (OrderedTypeSet(first) == OrderedTypeSet(second)) == result
+
+
+@pytest.mark.parametrize(
+    "first, second, result",
+    [([], [], []), ([int], [], [int]), ([], [int], [int]), ([int], [float], [int, float])],
+)
+def test_orderedtypeset_or_union(first, second, result):
+    assert OrderedTypeSet(first) | OrderedTypeSet(second) == OrderedTypeSet(result)
+    assert OrderedTypeSet(first).union(OrderedTypeSet(second)) == OrderedTypeSet(result)
+
+
+@pytest.mark.parametrize(
+    "first, second, result",
+    [([], [], []), ([int], [], []), ([], [int], []), ([int], [float], []), ([int, float], [float, str], [float])],
+)
+def test_orderedtypeset_and_intersection(first, second, result):
+    assert OrderedTypeSet(first) & OrderedTypeSet(second) == OrderedTypeSet(result)
+    assert OrderedTypeSet(first).intersection(OrderedTypeSet(second)) == OrderedTypeSet(result)
+
+
 def test_ordered_type_set_add():
     ordered_type_set = OrderedTypeSet()
     ordered_type_set.add(float | int)
@@ -93,3 +145,113 @@ def test_ordered_type_set_union():
     union = ordered_type_set_1 | ordered_type_set_2
     assert str in union
     assert int in union
+
+
+def test_ordered_type_set_init():
+    ordered_type_set = OrderedTypeSet([int, float | str])
+    assert int in ordered_type_set
+    assert float in ordered_type_set
+    assert str in ordered_type_set
+
+
+def test_ordered_type_set_len():
+    ordered_type_set = OrderedTypeSet([int, float])
+    assert len(ordered_type_set) == 2
+
+
+def test_ordered_type_set_getitem():
+    ordered_type_set = OrderedTypeSet([int, float, str])
+    assert ordered_type_set[0] == int
+    assert ordered_type_set[1] == float
+    assert ordered_type_set[2] == str
+
+
+def test_ordered_type_set_getitem_slice():
+    ordered_type_set = OrderedTypeSet([int, float, str])
+    with pytest.raises(NotImplementedError):
+        ordered_type_set[1:2]
+
+
+def test_ordered_type_set_eq():
+    assert OrderedTypeSet([int, float]) == OrderedTypeSet([int, float])
+    assert OrderedTypeSet([int, float]) != OrderedTypeSet([str, float])
+
+
+def test_ordered_type_set_eq_other_type():
+    assert not OrderedTypeSet([int, float]) == "foo"
+
+
+def test_ordered_type_set_clear():
+    ordered_type_set = OrderedTypeSet([int, float])
+    ordered_type_set.clear()
+    assert len(ordered_type_set) == 0
+
+
+def test_ordered_type_set_intersection():
+    ordered_type_set_1 = OrderedTypeSet([int, float, str])
+    ordered_type_set_2 = OrderedTypeSet([float, str])
+    intersection = ordered_type_set_1 & ordered_type_set_2
+    assert float in intersection
+    assert str in intersection
+    assert int not in intersection
+
+
+def test_ordered_type_set_difference():
+    ordered_type_set_1 = OrderedTypeSet([int, float, str])
+    ordered_type_set_2 = OrderedTypeSet([str])
+    difference = ordered_type_set_1.difference(ordered_type_set_2)
+    assert int in difference
+    assert float in difference
+    assert str not in difference
+
+
+def test_ordered_type_set_symmetric_difference():
+    ordered_type_set_1 = OrderedTypeSet([int, float])
+    ordered_type_set_2 = OrderedTypeSet([float, str])
+    sym_diff = ordered_type_set_1 ^ ordered_type_set_2
+    assert int in sym_diff
+    assert str in sym_diff
+    assert float not in sym_diff
+
+
+def test_ordered_type_set_issubset():
+    ordered_type_set_1 = OrderedTypeSet([int, float])
+    ordered_type_set_2 = OrderedTypeSet([int, float, str])
+    assert ordered_type_set_1.issubset(ordered_type_set_2)
+    assert not ordered_type_set_2.issubset(ordered_type_set_1)
+
+
+def test_ordered_type_set_issuperset():
+    ordered_type_set_1 = OrderedTypeSet([int, float, str])
+    ordered_type_set_2 = OrderedTypeSet([int, float])
+    assert ordered_type_set_1.issuperset(ordered_type_set_2)
+    assert not ordered_type_set_2.issuperset(ordered_type_set_1)
+
+
+def test_ordered_type_set_difference_update():
+    ordered_type_set = OrderedTypeSet([int, float, str])
+    ordered_type_set.difference_update([float, str])
+    assert int in ordered_type_set
+    assert float not in ordered_type_set
+    assert str not in ordered_type_set
+
+
+def test_ordered_type_set_intersection_update():
+    ordered_type_set = OrderedTypeSet([int, float, str])
+    ordered_type_set.intersection_update([float, str])
+    assert float in ordered_type_set
+    assert str in ordered_type_set
+    assert int not in ordered_type_set
+
+
+def test_ordered_type_set_symmetric_difference_update():
+    ordered_type_set = OrderedTypeSet([int, float])
+    ordered_type_set.symmetric_difference_update([float, str])
+    assert int in ordered_type_set
+    assert str in ordered_type_set
+    assert float not in ordered_type_set
+
+
+def test_ordered_type_set_repr():
+    ordered_type_set = OrderedTypeSet([int, float])
+    assert repr(ordered_type_set) == "OrderedTypeSet([<class 'int'>, <class 'float'>])"
