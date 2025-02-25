ccc1c245a869ae95a0be571df543b00f5482bf5e
Stephan Lukasczyk <stephan@pynguin.eu>
2025-02-25 21:07:16 +0100

    Provide ability to dump CLI parameters
    Allows to dump the CLI parameters in a way that the resulting file can
    directly be used again as an CLI argument to Pynguin.

--- src/pynguin/utils/configuration_writer.py
+++ src/pynguin/utils/configuration_writer.py
@@ -6,16 +6,19 @@
 #
 """Utility to write the current configuration to a TOML file."""
 
+import dataclasses
 import json
 import pprint
 
 from pathlib import Path
+from typing import Any
 
 import toml
 
 import pynguin.configuration as config
 
 
+PYNGUIN_CLI_PARAMS = "pynguin-cli-params.txt"
 PYNGUIN_CONFIG_TXT = "pynguin-config.txt"
 PYNGUIN_CONFIG_TOML = "pynguin-config.toml"
 
@@ -35,6 +38,10 @@ def write_configuration():
         txt_file = report_dir / PYNGUIN_CONFIG_TXT
         txt_file.write_text(pprint.pformat(repr(config.configuration)))
 
+        # Write CLI options to a TXT file
+        cli_file = report_dir / PYNGUIN_CLI_PARAMS
+        cli_file.write_text(" ".join(extract_parameter_list_from_config()))
+
 
 def convert_config_to_dict(config_obj: object) -> dict[str, str | dict[str, str]]:
     """Converts a configuration object to a dictionary.
@@ -46,3 +53,42 @@ def convert_config_to_dict(config_obj: object) -> dict[str, str | dict[str, str]
         A dictionary representation of the configuration object.
     """
     return json.loads(json.dumps(config_obj, default=lambda o: o.__dict__))
+
+
+def extract_parameter_list_from_config() -> list[str]:
+    """Extracts the CLI parameter list from the current configuration.
+
+    Allows to use the dumped configuration as input for the CLI again.  Pynguin's CLI parser already
+    supports the loading of configuration options from a file, as specified in the `argparse`
+    documentation.
+
+    Implementation detail: the result is sorted to make the output deterministic.
+
+    Returns:
+        A sorted list of CLI parameters.
+    """
+
+    def format_parameter(k: str, v: Any) -> str:
+        if isinstance(v, list):
+            return f"--{k} {','.join(v)}"
+        return f"--{k} {v}"
+
+    parameter_list: list[str] = []
+    cfg_dict = dataclasses.asdict(config.configuration)
+
+    for key, value in cfg_dict.items():
+        if isinstance(value, str) and value:
+            parameter_list.append(format_parameter(key, value))
+        elif isinstance(value, dict):
+            for sub_key, sub_value in value.items():
+                if isinstance(sub_value, str) and not sub_value:
+                    continue
+                if isinstance(sub_value, dict):
+                    for sub_sub_key, sub_sub_value in sub_value.items():
+                        parameter_list.append(
+                            format_parameter(f"{sub_key}.{sub_sub_key}", sub_sub_value)
+                        )
+                else:
+                    parameter_list.append(format_parameter(f"{sub_key}", sub_value))
+
+    return sorted(parameter_list)
--- tests/utils/test_configuration_writer.py
+++ tests/utils/test_configuration_writer.py
@@ -4,6 +4,7 @@
 #
 #  SPDX-License-Identifier: MIT
 #
+
 from pathlib import Path
 
 import pytest
@@ -14,6 +15,7 @@ from pynguin.configuration import StatisticsBackend
 from pynguin.utils.configuration_writer import PYNGUIN_CONFIG_TOML
 from pynguin.utils.configuration_writer import PYNGUIN_CONFIG_TXT
 from pynguin.utils.configuration_writer import convert_config_to_dict
+from pynguin.utils.configuration_writer import extract_parameter_list_from_config
 from pynguin.utils.configuration_writer import write_configuration
 
 
@@ -219,6 +221,109 @@ def expected_txt(tmp_path):
     return expected_txt
 
 
+@pytest.fixture
+def expected_parameter_list() -> list[str]:
+    parameter_list: list[str] = [
+        "--project_path /tmp",
+        "--module_name dummy",
+        "--export_strategy PY_TEST",
+        "--max_length_test_case 2500",
+        "--assertion_generation MUTATION_ANALYSIS",
+        "--allow_stale_assertions False",
+        "--mutation_strategy FIRST_ORDER_MUTANTS",
+        "--mutation_order 1",
+        "--post_process True",
+        "--float_precision 0.01",
+        "--format_with_black True",
+        "--algorithm RANDOM",  # as defined in tests/conftest.py
+        "--report_dir pynguin-report",
+        "--statistics_backend CSV",
+        "--timeline_interval 1000000000",
+        "--timeline_interpolation True",
+        "--coverage_metrics BRANCH",
+        "--output_variables TargetModule,Coverage",
+        "--create_coverage_report False",
+        "--type_guess_top_n 10",
+        "--maximum_search_time -1",
+        "--maximum_test_executions -1",
+        "--maximum_statement_executions -1",
+        "--maximum_slicing_time 600",
+        "--maximum_iterations -1",
+        "--maximum_test_execution_timeout 5",
+        "--maximum_coverage 100",
+        "--maximum_coverage_plateau -1",
+        "--minimum_coverage 100",
+        "--minimum_plateau_iterations -1",
+        "--test_execution_time_per_statement 1",
+        "--seed 12345",
+        "--constant_seeding True",
+        "--initial_population_seeding False",
+        "--seeded_testcases_reuse_probability 0.9",
+        "--initial_population_mutations 0",
+        "--dynamic_constant_seeding True",
+        "--seeded_primitives_reuse_probability 0.2",
+        "--seeded_dynamic_values_reuse_probability 0.6",
+        "--seed_from_archive False",
+        "--seed_from_archive_probability 0.2",
+        "--seed_from_archive_mutations 3",
+        "--max_dynamic_length 1000",
+        "--max_dynamic_pool_size 50",
+        "--type_inference_strategy TYPE_HINTS",
+        "--type_tracing False",
+        "--max_recursion 10",
+        "--max_delta 20",
+        "--max_int 2048",
+        "--string_length 20",
+        "--bytes_length 20",
+        "--collection_size 5",
+        "--primitive_reuse_probability 0.5",
+        "--object_reuse_probability 0.9",
+        "--none_weight 0",
+        "--any_weight 0",
+        "--original_type_weight 5",
+        "--type_tracing_weight 10",
+        "--type4py_weight 10",
+        "--type_tracing_kept_guesses 2",
+        "--wrap_var_param_type_probability 0.7",
+        "--negate_type 0.1",
+        "--skip_optional_parameter_probability 0.7",
+        "--max_attempts 1000",
+        "--insertion_uut 0.5",
+        "--max_size 100",
+        "--use_random_object_for_call 0.0",
+        "--min_initial_tests 1",
+        "--max_initial_tests 10",
+        "--population 50",
+        "--chromosome_length 40",
+        "--chop_max_length True",
+        "--elite 1",
+        "--crossover_rate 0.75",
+        "--test_insertion_probability 0.1",
+        "--test_delete_probability 0.3333333333333333",
+        "--test_change_probability 0.3333333333333333",
+        "--test_insert_probability 0.3333333333333333",
+        "--statement_insertion_probability 0.5",
+        "--random_perturbation 0.2",
+        "--change_parameter_probability 0.1",
+        "--tournament_size 5",
+        "--rank_bias 1.7",
+        "--selection TOURNAMENT_SELECTION",
+        "--use_archive False",
+        "--filter_covered_targets_from_test_cluster False",
+        "--number_of_mutations 1",
+        "--exploitation_starts_at_percent 0.5",
+        "--initial_config.number_of_tests_per_target 10",
+        "--initial_config.random_test_or_from_archive_probability 0.5",
+        "--initial_config.number_of_mutations 1",
+        "--focused_config.number_of_tests_per_target 1",
+        "--focused_config.random_test_or_from_archive_probability 0.0",
+        "--focused_config.number_of_mutations 10",
+        "--max_sequence_length 10",
+        "--max_sequences_combined 10",
+    ]
+    return sorted(parameter_list)
+
+
 def test_write_configuration(expected_toml, expected_txt, tmp_path):
     config.configuration.statistics_output.statistics_backend = StatisticsBackend.CSV
     config.configuration.statistics_output.report_dir = str(tmp_path)
@@ -277,3 +382,13 @@ def test_ignore_callable_and_dunder_methods():
     assert "_hidden" in converted
     assert "__private" not in converted
     assert "method" not in converted
+
+
+def test(expected_parameter_list):
+    config.configuration.module_name = "dummy"
+    config.configuration.project_path = "/tmp"  # noqa: S108
+    config.configuration.seeding.seed = 12345
+
+    parameter_list = extract_parameter_list_from_config()
+
+    assert parameter_list == expected_parameter_list
