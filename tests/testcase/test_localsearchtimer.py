efa986cc70f05129e458060fb376d0d0d27e5d5f
Alex Wuest <alex.wuest@freenetmail.de>
2025-06-13 13:27:36 +0200

    fixed singleton reset in tests

--- tests/testcase/test_localsearchtimer.py
+++ tests/testcase/test_localsearchtimer.py
@@ -4,22 +4,27 @@
 #
 #  SPDX-License-Identifier: MIT
 #
+from unittest.mock import patch
+
 from pynguin.configuration import configuration
 from pynguin.testcase.localsearch import LocalSearchTimer
 
 
 def test_timer_limit_reached(monkeypatch, result) -> None:
-    monkeypatch.setattr("pynguin.config.LocalSearchConfiguration.local_search_time", -1)
-    timer = LocalSearchTimer.get_instance()
-    timer.start_local_search()
-    assert timer.limit_reached()
+    with patch.object(LocalSearchTimer, "_instance", None):
+        monkeypatch.setattr("pynguin.config.LocalSearchConfiguration.local_search_time", -1)
+        timer = LocalSearchTimer.get_instance()
+        timer.start_local_search()
+        assert timer.limit_reached()
 
 def test_timer_limit_not_reached(monkeypatch, result) -> None:
-    monkeypatch.setattr("pynguin.config.LocalSearchConfiguration.local_search_time", 1000000000)
-    timer = LocalSearchTimer.get_instance()
-    timer.start_local_search()
-    assert not timer.limit_reached()
+    with patch.object(LocalSearchTimer, "_instance", None):
+        monkeypatch.setattr("pynguin.config.LocalSearchConfiguration.local_search_time", 1000000000)
+        timer = LocalSearchTimer.get_instance()
+        timer.start_local_search()
+        assert not timer.limit_reached()
 
 def test_timer_not_started(monkeypatch, result) -> None:
-    timer = LocalSearchTimer.get_instance()
-    assert timer.limit_reached()
+    with patch.object(LocalSearchTimer, "_instance", None):
+        timer = LocalSearchTimer.get_instance()
+        assert timer.limit_reached()
