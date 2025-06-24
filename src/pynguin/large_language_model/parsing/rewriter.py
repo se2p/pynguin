e7f5dd8819b461bd21d6872ed7367214eaa587e4
Stephan Lukasczyk <stephan@pynguin.eu>
2025-06-24 11:37:03 +0200

    Update the copyright headers of CodaMosa-adopted components

--- src/pynguin/large_language_model/parsing/rewriter.py
+++ src/pynguin/large_language_model/parsing/rewriter.py
@@ -1,6 +1,7 @@
 # This file is part of Pynguin.
 #
-# SPDX-FileCopyrightText: 2019–2024 Pynguin Contributors
+# SPDX-FileCopyrightText: 2019–2025 Pynguin Contributors
+# SPDX-FileCopyrightText: 2023 Microsoft
 #
 # SPDX-License-Identifier: MIT
 #
