a6e29090937ca41e0e04e38c36afde8915cdf8e3
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2025-09-19 07:17:21 +0200

    Fix test_ast_info

--- tests/instrumentation/test_ast_info.py
+++ tests/instrumentation/test_ast_info.py
@@ -4,14 +4,44 @@
 #
 #  SPDX-License-Identifier: MIT
 #
+import importlib.util
+
+from pathlib import Path
+
+import pytest
+
 from pynguin.instrumentation.transformer import ModuleAstInfo
 
 
 def get_module_path(module_name: str, extension: str = ".py") -> str:
-    return module_name.replace(".", "/") + extension
+    """Return absolute path to a module.
+
+    It requires the module to be in a package and it does not work for __init__ modules.
+
+    Args:
+        module_name: Dotted module path.
+        extension: File extension.
+
+    Returns:
+        Absolute path to the module file.
+    """
+    package_name, *submodule_names = module_name.split(".")
+    package_spec = importlib.util.find_spec(package_name)
+    assert package_spec is not None
+    assert package_spec.origin is not None
+    return str(Path(package_spec.origin).parent.joinpath(*submodule_names).with_suffix(extension))
 
 
-def test_ast_info_from_covered_function():
+@pytest.mark.parametrize(
+    "scope_line, expected_should_be_covered",
+    [
+        (8, False),
+        (14, False),
+        (20, False),
+        (27, True),
+    ],
+)
+def test_ast_info_from_covered_function(scope_line, expected_should_be_covered):
     module_name = "tests.fixtures.instrumentation.covered_functions"
     module_ast_info = ModuleAstInfo.from_path(
         get_module_path(module_name),
@@ -26,24 +56,21 @@ def test_ast_info_from_covered_function():
     assert not module_ast_info.only_cover_lines
     assert module_ast_info.no_cover_lines == {8, 14, 20}
 
-    not_covered1 = module_ast_info.get_scope(8)
-    assert not_covered1 is not None
-    assert not not_covered1.should_be_covered()
-
-    not_covered2 = module_ast_info.get_scope(14)
-    assert not_covered2 is not None
-    assert not not_covered2.should_be_covered()
-
-    not_covered3 = module_ast_info.get_scope(20)
-    assert not_covered3 is not None
-    assert not not_covered3.should_be_covered()
-
-    covered = module_ast_info.get_scope(27)
-    assert covered is not None
-    assert covered.should_be_covered()
+    scope = module_ast_info.get_scope(scope_line)
+    assert scope is not None
+    assert scope.should_be_covered() is expected_should_be_covered
 
 
-def test_ast_info_from_covered_function_no_cover():
+@pytest.mark.parametrize(
+    "scope_line, expected_should_be_covered",
+    [
+        (8, True),
+        (14, True),
+        (20, True),
+        (27, False),
+    ],
+)
+def test_ast_info_from_covered_function_no_cover(scope_line, expected_should_be_covered):
     module_name = "tests.fixtures.instrumentation.covered_functions"
     module_ast_info = ModuleAstInfo.from_path(
         get_module_path(module_name),
@@ -58,24 +85,21 @@ def test_ast_info_from_covered_function_no_cover():
     assert not module_ast_info.only_cover_lines
     assert module_ast_info.no_cover_lines == {27}
 
-    not_covered1 = module_ast_info.get_scope(8)
-    assert not_covered1 is not None
-    assert not_covered1.should_be_covered()
-
-    not_covered2 = module_ast_info.get_scope(14)
-    assert not_covered2 is not None
-    assert not_covered2.should_be_covered()
-
-    not_covered3 = module_ast_info.get_scope(20)
-    assert not_covered3 is not None
-    assert not_covered3.should_be_covered()
-
-    covered = module_ast_info.get_scope(27)
-    assert covered is not None
-    assert not covered.should_be_covered()
+    scope = module_ast_info.get_scope(scope_line)
+    assert scope is not None
+    assert scope.should_be_covered() is expected_should_be_covered
 
 
-def test_ast_info_from_covered_function_only_cover():
+@pytest.mark.parametrize(
+    "scope_line, expected_should_be_covered",
+    [
+        (8, True),
+        (14, False),
+        (20, False),
+        (27, False),
+    ],
+)
+def test_ast_info_from_covered_function_only_cover(scope_line, expected_should_be_covered):
     module_name = "tests.fixtures.instrumentation.covered_functions"
     module_ast_info = ModuleAstInfo.from_path(
         get_module_path(module_name),
@@ -90,24 +114,20 @@ def test_ast_info_from_covered_function_only_cover():
     assert module_ast_info.only_cover_lines == {8}
     assert not module_ast_info.no_cover_lines
 
-    not_covered1 = module_ast_info.get_scope(8)
-    assert not_covered1 is not None
-    assert not_covered1.should_be_covered()
-
-    not_covered2 = module_ast_info.get_scope(14)
-    assert not_covered2 is not None
-    assert not not_covered2.should_be_covered()
-
-    not_covered3 = module_ast_info.get_scope(20)
-    assert not_covered3 is not None
-    assert not not_covered3.should_be_covered()
-
-    covered = module_ast_info.get_scope(27)
-    assert covered is not None
-    assert not covered.should_be_covered()
+    scope = module_ast_info.get_scope(scope_line)
+    assert scope is not None
+    assert scope.should_be_covered() is expected_should_be_covered
 
 
-def test_ast_info_from_covered_classes():
+@pytest.mark.parametrize(
+    "scope_line, expected_should_be_covered",
+    [
+        (9, False),
+        (13, False),
+        (17, True),
+    ],
+)
+def test_ast_info_from_covered_classes(scope_line, expected_should_be_covered):
     module_name = "tests.fixtures.instrumentation.covered_classes"
     module_ast_info = ModuleAstInfo.from_path(
         get_module_path(module_name),
@@ -122,20 +142,25 @@ def test_ast_info_from_covered_classes():
     assert not module_ast_info.only_cover_lines
     assert module_ast_info.no_cover_lines == {8, 13}
 
-    foo_foo = module_ast_info.get_scope(9)
-    assert foo_foo is not None
-    assert not foo_foo.should_be_covered()
-
-    bar_bar = module_ast_info.get_scope(13)
-    assert bar_bar is not None
-    assert not bar_bar.should_be_covered()
-
-    baz_baz = module_ast_info.get_scope(17)
-    assert baz_baz is not None
-    assert baz_baz.should_be_covered()
+    scope = module_ast_info.get_scope(scope_line)
+    assert scope is not None
+    assert scope.should_be_covered() is expected_should_be_covered
 
 
-def test_ast_info_from_covered_branches():  # noqa: PLR0915
+@pytest.mark.parametrize(
+    "scope_line, expected_lines",
+    [
+        # scope_line, dict of line: expected_should_cover
+        (8, {9: False, 10: False, 11: True, 12: True}),
+        (14, {15: True, 16: True, 17: False, 18: False, 19: True, 20: True}),
+        (22, {23: True, 24: True, 25: False, 26: False}),
+        (28, {29: False, 30: False, 31: False, 32: False, 33: False, 34: True, 35: True}),
+        (37, {38: True, 39: False, 40: False, 41: True, 42: True, 43: True, 44: True}),
+        (46, {47: False, 48: False, 49: True}),
+        (51, {52: False, 53: False, 54: True}),
+    ],
+)
+def test_ast_info_from_covered_branches(scope_line, expected_lines):
     module_name = "tests.fixtures.instrumentation.covered_branches"
     module_ast_info = ModuleAstInfo.from_path(
         get_module_path(module_name),
@@ -150,70 +175,22 @@ def test_ast_info_from_covered_branches():  # noqa: PLR0915
     assert not module_ast_info.only_cover_lines
     assert module_ast_info.no_cover_lines == {9, 17, 25, 29, 39, 47, 52}
 
-    if_x = module_ast_info.get_scope(8)
-    assert if_x is not None
-    assert if_x.should_be_covered()
-    assert not if_x.should_cover_line(9)
-    assert not if_x.should_cover_line(10)
-    assert if_x.should_cover_line(11)
-    assert if_x.should_cover_line(12)
+    scope = module_ast_info.get_scope(scope_line)
+    assert scope is not None
+    assert scope.should_be_covered()
 
-    elif_x = module_ast_info.get_scope(14)
-    assert elif_x is not None
-    assert elif_x.should_be_covered()
-    assert elif_x.should_cover_line(15)
-    assert elif_x.should_cover_line(16)
-    assert not elif_x.should_cover_line(17)
-    assert not elif_x.should_cover_line(18)
-    assert elif_x.should_cover_line(19)
-    assert elif_x.should_cover_line(20)
-
-    else_x = module_ast_info.get_scope(22)
-    assert else_x is not None
-    assert else_x.should_be_covered()
-    assert else_x.should_cover_line(23)
-    assert else_x.should_cover_line(24)
-    assert not else_x.should_cover_line(25)
-    assert not else_x.should_cover_line(26)
-
-    nested_if_x = module_ast_info.get_scope(28)
-    assert nested_if_x is not None
-    assert nested_if_x.should_be_covered()
-    assert not nested_if_x.should_cover_line(29)
-    assert not nested_if_x.should_cover_line(30)
-    assert not nested_if_x.should_cover_line(31)
-    assert not nested_if_x.should_cover_line(32)
-    assert not nested_if_x.should_cover_line(33)
-    assert nested_if_x.should_cover_line(34)
-    assert nested_if_x.should_cover_line(35)
-
-    nested_if_y = module_ast_info.get_scope(37)
-    assert nested_if_y is not None
-    assert nested_if_y.should_be_covered()
-    assert nested_if_y.should_cover_line(38)
-    assert not nested_if_y.should_cover_line(39)
-    assert not nested_if_y.should_cover_line(40)
-    assert nested_if_y.should_cover_line(41)
-    assert nested_if_y.should_cover_line(42)
-    assert nested_if_y.should_cover_line(43)
-    assert nested_if_y.should_cover_line(44)
-
-    while_x = module_ast_info.get_scope(46)
-    assert while_x is not None
-    assert while_x.should_be_covered()
-    assert not while_x.should_cover_line(47)
-    assert not while_x.should_cover_line(48)
-    assert while_x.should_cover_line(49)
-
-    for_i = module_ast_info.get_scope(51)
-    assert for_i is not None
-    assert for_i.should_be_covered()
-    assert not for_i.should_cover_line(52)
-    assert not for_i.should_cover_line(53)
-    assert for_i.should_cover_line(54)
+    for line, expected in expected_lines.items():
+        assert scope.should_cover_line(line) is expected
 
 
-def test_ast_info_from_covered_lines():
+@pytest.mark.parametrize(
+    "scope_line, expected_lines",
+    [
+        # scope_line, dict of line: expected_should_cover
+        (8, {9: True, 10: False, 12: True, 13: True, 14: True, 15: True, 16: False, 18: True}),
+    ],
+)
+def test_ast_info_from_covered_lines(scope_line, expected_lines):
     module_name = "tests.fixtures.instrumentation.covered_lines"
     module_ast_info = ModuleAstInfo.from_path(
         get_module_path(module_name),
@@ -228,17 +205,12 @@ def test_ast_info_from_covered_lines():
     assert not module_ast_info.only_cover_lines
     assert module_ast_info.no_cover_lines == {10, 16}
 
-    no_cover_lines = module_ast_info.get_scope(8)
-    assert no_cover_lines is not None
-    assert no_cover_lines.should_be_covered()
-    assert no_cover_lines.should_cover_line(9)
-    assert not no_cover_lines.should_cover_line(10)
-    assert no_cover_lines.should_cover_line(12)
-    assert no_cover_lines.should_cover_line(13)
-    assert no_cover_lines.should_cover_line(14)
-    assert no_cover_lines.should_cover_line(15)
-    assert not no_cover_lines.should_cover_line(16)
-    assert no_cover_lines.should_cover_line(18)
+    scope = module_ast_info.get_scope(scope_line)
+    assert scope is not None
+    assert scope.should_be_covered()
+
+    for line, expected in expected_lines.items():
+        assert scope.should_cover_line(line) is expected
 
 
 def test_ast_info_from_invalid():
