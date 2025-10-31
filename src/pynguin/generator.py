b1a59188c4086b5d6ffd9dab006c9f1752190ecf
Berg Lucas <55436804+BergLucas@users.noreply.github.com>
2025-10-31 14:39:45 +0100

    Disable the tracer before exporting tests and generating the coverage report

--- src/pynguin/generator.py
+++ src/pynguin/generator.py
@@ -609,6 +609,8 @@ def _run() -> ReturnCode:  # noqa: C901
         algorithm, executor, generation_result, constant_provider
     )
 
+    executor.subject_properties.instrumentation_tracer.disable()
+
     # Export the generated test suites
     if config.configuration.test_case_output.export_strategy == config.ExportStrategy.PY_TEST:
         try:
